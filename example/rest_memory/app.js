a1c87ce59d5140334c18de62b4f9a7e4f5921932
Eric Kryski <e.kryski@gmail.com>
2013-04-29 01:58:44 -0600

    wrappin express' .use, .set, .get. Sorting out some config options

--- example/rest_memory/app.js
+++ example/rest_memory/app.js
@@ -1,10 +1,12 @@
 var feathry = require('../../lib/feathry');
 var Proto = require('uberproto');
 var memoryService = feathry.memory();
+var express = require('express');
 
 Proto.mixin(require('../../lib/mixin/event'), memoryService);
 
-feathry.createServer({ port: 8000 })
+feathry.createServer()
+  .use(express.static(__dirname))
   .service('users', memoryService)
   .provide(feathry.rest())
   .start();
