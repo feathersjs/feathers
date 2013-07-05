9ebaa860a065b0a7e9080fef654557f10163b49b
Eric Kryski <e.kryski@gmail.com>
2013-07-04 20:14:34 -0600

    namespaced services and renamed mongo service. Working on a more complex example

--- example/rest_mongo/index.js
+++ example/rest_mongo/index.js
@@ -1,23 +1,25 @@
 var feathers = require('../../lib/feathers');
 var Proto = require('uberproto');
 
-var options = {
-  // username: 'hulk',
-  // password: 'greenman',
-  // host: 'localhost',
-  // port: '27017',
-  // reconnect: true,
-  db: 'feathers'
+var userOptions = {
+  collection: 'users'
 };
 
-var mongoService = feathers.mongo(options);
+var postOptions = {
+  collection: 'posts'
+};
+
+var userService = feathers.service.mongodb(userOptions);
+var postService = feathers.service.mongodb(postOptions);
 var express = require('express');
 
-Proto.mixin(require('../../lib/mixins/event'), mongoService);
+Proto.mixin(require('../../lib/mixins/event'), userService);
+Proto.mixin(require('../../lib/mixins/event'), postService);
 
 feathers.createServer()
   .use(express.static(__dirname))
-  .service('users', mongoService)
+  .service('users', userService)
+  .service('posts', postService)
   .provide(feathers.rest())
   .provide(feathers.socketio())
   .start();
