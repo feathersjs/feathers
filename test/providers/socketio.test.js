4a50abeaa3cc227b3ac2a696ada85c0df80e2e76
David Luecke <daff@neyeon.com>
2013-07-08 23:25:01 -0600

    Some more SocketIO tests and figuring out how to close the server

--- test/providers/socketio.test.js
+++ test/providers/socketio.test.js
@@ -1,16 +1,42 @@
-var assert = require('assert');
+var expect = require('chai').expect;
 var request = require('request');
 var feathers = require('../../lib/feathers');
 var io = require('socket.io-client');
+var _ = require('underscore');
 
 describe('SocketIO provider', function () {
-	it('get', function (done) {
+//	it('get', function (done) {
+//		var todoService = {
+//			get: function(name, params, callback) {
+//				callback(null, {
+//					id: name,
+//					description: 'You have to do ' + name + '!'
+//				});
+//			}
+//		};
+//
+//		var server = feathers.createServer({ port: 8000 })
+//			.service('todo', todoService)
+//			.provide(feathers.socketio())
+//			.start();
+//
+//		var socket = io.connect('http://localhost:8000');
+//
+//		socket.emit('todo::get', 'dishes', {}, function(error, data) {
+//			expect(error).to.be.null;
+//			expect(data.id).to.equal('dishes');
+//			expect(data.description).to.equal('You have to do dishes!');
+//			server.stop();
+//			done();
+//		});
+//	});
+
+	it('create and created event', function (done) {
 		var todoService = {
-			get: function(name, params, callback) {
-				callback(null, {
-					id: name,
-					description: "You have to do " + name + "!"
-				});
+			create: function(data, params, callback) {
+				_.defer(function() {
+					callback(null, data);
+				}, 200);
 			}
 		};
 
@@ -21,9 +47,18 @@ describe('SocketIO provider', function () {
 
 		var socket = io.connect('http://localhost:8000');
 
-		socket.emit('todo::get', 'dishes', {}, function(error, data) {
-			console.log(data);
+		socket.on('todo created', function(data) {
+			expect(data.id).to.equal(1);
+			expect(data.name).to.equal('Create dishes');
+			server.stop();
 			done();
+		});
+
+		socket.emit('todo::create', {
+			id: 1,
+			name: 'Create dishes'
+		}, {}, function(error, data) {
+			expect(error).to.be.null;
 		});
 	});
 });
