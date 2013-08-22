12de3a8c15f997978a2a009da17f4d469773bd35
David Luecke <daff@neyeon.com>
2013-08-21 21:11:09 -0600

    Initial refactoring to extend Express directly

--- example/rest_memory/index.js
+++ example/rest_memory/index.js
@@ -1,10 +1,9 @@
 var feathers = require('../../lib/feathers');
-var Proto = require('uberproto');
 var memoryService = feathers.service.memory();
 var express = require('express');
 
-feathers.createServer({ port: 3000 })
+feathers()
   .use(express.static(__dirname))
   .service('users', memoryService)
   .provide(feathers.rest())
-  .start();
+  .listen(3000);
