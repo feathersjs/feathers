127e0094d36e42c5f4d7cbeb440b2bc22f7c34d8
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-02-15 18:23:47 -0400

    Closes #1. First working version.

---
+++ packages/generator-feathers-plugin/app/templates/Gruntfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    release: {},
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      lib: ['lib/**/*.js', 'Gruntfile.js'],
+      test: 'test/**/*.js'
+    },
+    simplemocha: {
+      all: {
+        src: ['test/**/*.test.js']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-release');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+
+  grunt.registerTask('test', 'simplemocha');
+  grunt.registerTask('default', ['jshint', 'simplemocha']);
+};
---
+++ packages/generator-feathers-plugin/app/templates/docs/README.md
@@ -0,0 +1,3 @@
+# Documentation
+
+Coming soon.
