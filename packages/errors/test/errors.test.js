9137ba3972ccbeb0588dd39d082ed8192d61728d
Eric Kryski <e.kryski@gmail.com>
2014-06-03 22:15:11 -0600

    adding tests

--- packages/errors/test/errors.test.js
+++ packages/errors/test/errors.test.js
@@ -1,14 +1,42 @@
 'use strict';
 
-var assert = require('assert');
+/* jshint undef:false */
 
+var assert = require('assert');
 var feathers = require('feathers');
-var error = require('../lib/errors');
+var errors = require('../lib/errors');
+var app;
 
 describe('Feathers errors', function () {
-  it('initializes .associate', function () {
-    var app = feathers().configure(errors());
 
-    assert.equal(typeof app.error, 'function', 'error method got added');
+  beforeEach(function() {
+    app = feathers().configure(errors());
+  });
+
+  it('initializes errors object', function () {
+    assert.equal(typeof app.errors, 'object', 'errors got added to the app');
+  });
+
+  it('can create app errors', function () {
+    var error = new app.errors.GeneralError('foo');
+
+    assert.equal(error.code, 500 ,'a general error was created with the correct code');
+    assert.equal(error.message, 'foo' ,'a general error was created with the correct message');
+  });
+
+  it('has all the available errors', function () {
+    assert.notEqual(typeof app.errors.GeneralError, 'undefined', 'has GeneralError');
+    assert.notEqual(typeof app.errors.BadRequest, 'undefined', 'has BadRequest');
+    assert.notEqual(typeof app.errors.NotAuthenticated, 'undefined', 'has NotAuthenticated');
+    assert.notEqual(typeof app.errors.Forbidden, 'undefined', 'has Forbidden');
+    assert.notEqual(typeof app.errors.NotFound, 'undefined', 'has NotFound');
+    assert.notEqual(typeof app.errors.Timeout, 'undefined', 'has Timeout');
+    assert.notEqual(typeof app.errors.Conflict, 'undefined', 'has Conflict');
+    assert.notEqual(typeof app.errors.PaymentError, 'undefined', 'has PaymentError');
+    assert.notEqual(typeof app.errors.Unprocessable, 'undefined', 'has Unprocessable');
+  });
+
+  it.skip('error handler catches errors', function () {
+    // TODO (EK)
   });
 });
