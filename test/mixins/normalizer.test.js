4fbf8080576726ceac816a253b5b3452b4b23b5d
David Luecke <daff@neyeon.com>
2015-03-07 12:14:55 -0700

    Adding normalizer mixin to always normalize service method calls.

--- test/mixins/normalizer.test.js
+++ test/mixins/normalizer.test.js
@@ -3,9 +3,25 @@
 var assert = require('assert');
 var Proto = require('uberproto');
 var normalizer = require('../../lib/mixins/normalizer');
+var mixins = require('../../lib/mixins');
 
 describe('Argument normalizer mixin', function () {
-  it('normalizes findAll', function (done) {
+  it('normalizer mixin is always the last to run', function() {
+    var arr = mixins();
+    var dummy = function() { };
+
+    assert.equal(arr.length, 3);
+
+    arr.push(dummy);
+
+    assert.equal(arr[arr.length - 1], normalizer, 'Last mixin is still the normalizer');
+    assert.equal(arr[arr.length - 2], dummy, 'Dummy got added before last');
+  });
+
+  // The normalization is already tests in all variations in `getArguments`
+  // so we just so we only test two random samples
+
+  it('normalizes .find without a callback', function (done) {
     var context = {
       methods: ['find']
     };
@@ -22,5 +38,26 @@ describe('Argument normalizer mixin', function () {
     var instance = Proto.create.call(FixtureService);
 
     instance.find({ test: 'Here' });
+  });
+
+  it('normalizes .update without params and callback', function (done) {
+    var context = {
+      methods: ['update']
+    };
+    var FixtureService = Proto.extend({
+      update: function(id, data, params, callback) {
+        assert.equal(id, 1);
+        assert.ok(typeof callback === 'function');
+        assert.deepEqual(data, { test: 'Here' });
+        assert.deepEqual(params, {});
+        done();
+      }
+    });
+
+    normalizer.call(context, FixtureService);
+
+    var instance = Proto.create.call(FixtureService);
+
+    instance.update(1, { test: 'Here' });
   });
 });
