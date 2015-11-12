ce10120fd3a16902f35c415c83b662f0782a06cc
David Luecke <daff@neyeon.com>
2015-11-11 17:26:41 -0800

    Merge pull request #12 from feathersjs/debug-module
    Use debug module in generated plugin

--- packages/generator-feathers-plugin/app/templates/src/index.js
+++ packages/generator-feathers-plugin/app/templates/src/index.js
@@ -1,5 +1,9 @@
+import makeDebug from 'debug';
+
+const debug = makeDebug('<%= name %>');
+
 export default function() {
   return function() {
-    
+    debug('Initializing <%= name %> plugin');
   };
 }
