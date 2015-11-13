32d4fc054f5d9ba070033d3deff425d370d8054e
David Luecke <daff@neyeon.com>
2015-11-12 21:59:18 -0800

    Initial commit of working configuration module

---
+++ packages/configuration/src/index.js
@@ -0,0 +1,44 @@
+import fs from 'fs';
+import path from 'path';
+import makeDebug from 'debug';
+
+const debug = makeDebug('feathers:configuration');
+
+module.exports = function (root, configFolder = 'config') {
+  return function() {
+    const app = this;
+    const env = app.settings.env;
+    const config = require(path.join(root, configFolder, 'default.json'));
+
+    debug(`Initializing configuration for ${env} environment`);
+
+    // Dev is our default development. For everything else extend the default
+    if(env !== 'development') {
+      const envConfig = path.join(root, configFolder, `${env}.json`);
+      // We can use sync here since configuration only happens once at startup
+      if(fs.existsSync(envConfig)) {
+          Object.assign(config, require(envConfig));
+      } else {
+        debug(`Configuration file for ${env} environment not found at ${envConfig}`);
+      }
+    }
+
+    Object.keys(config).forEach(name => {
+      let value = config[name];
+
+      if(process.env[value]) {
+        value = process.env[value];
+      }
+
+      // Make relative paths absolute
+      if(typeof value === 'string' && (value.indexOf(`.${path.sep}`) === 0 ||
+          value.indexOf(`..${path.sep}`) === 0)) {
+        value = path.resolve(path.join(root, configFolder), value);
+      }
+
+      debug(`Setting ${name} configuration value to`, value);
+
+      app.set(name, value);
+    });
+  };
+};
---
+++ packages/configuration/test/index.test.js
@@ -0,0 +1,20 @@
+import assert from 'assert';
+import feathers from 'feathers';
+import { join } from 'path';
+import plugin from '../src';
+
+describe('feathers-configuration', () => {
+  const app = feathers().configure(plugin(__dirname));
+
+  it('initialized app with default.json data', () =>
+    assert.equal(app.get('port'), 3030));
+
+  it('initialized with <env>.json', () =>
+    assert.equal(app.get('from'), 'testing'));
+
+  it('initialized property with environment variable', () =>
+    assert.equal(app.get('environment'), 'testing'));
+
+  it('normalizes relative path names', () =>
+    assert.equal(app.get('path'), join(__dirname, 'something')));
+});
