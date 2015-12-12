53ba2069bbc9d50b9963d9a51956aee43fbc257c
Eric Kryski <e.kryski@gmail.com>
2015-12-12 01:21:27 -0700

    exporting errors by default

--- packages/errors/src/index.js
+++ packages/errors/src/index.js
@@ -144,54 +144,5 @@ var errors = {
 
 var types = errors;
 
-// // Abstract Error
-// let AbstractError = (msg, constr, data) => {
-//   Error.captureStackTrace(this, constr || this);
-
-//   if (msg instanceof Error) {
-//     this.message = msg.message;
-
-//     if (msg.errors) {
-//       this.errors = msg.errors;
-//     }
-//   }
-//   else {
-//     this.message = msg || 'Error';
-//   }
-  
-//   this.data = data;
-// };
-
-// util.inherits(AbstractError, Error);
-// AbstractError.prototype.name = 'Abstract Error';
-
-// let createError = (errorName, code, className) => {
-//   let errorFn = (msg, data) => {
-//     debug(`Feathers Error: ${errorName}(${code}): ${msg}`);
-//     errorFn.super_.call(this, msg, this.constructor, data);
-//   };
-
-//   util.inherits(errorFn, AbstractError);
-
-//   errorFn.prototype.name = errorName;
-//   errorFn.prototype.code = code;
-//   errorFn.prototype.className = className;
-
-//   errors[errorName] = errorFn;
-// };
-
-// createError('BadRequest', 400, 'bad-request');
-// createError('NotAuthenticated', 401, 'not-authenticated');
-// createError('PaymentError', 402, 'payment-error');
-// createError('Forbidden', 403, 'forbidden');
-// createError('NotFound', 404, 'not-found');
-// createError('MethodNotAllowed', 405, 'method-not-allowed');
-// createError('NotAcceptable', 406, 'not-acceptable');
-// createError('Timeout', 408, 'timeout');
-// createError('Conflict', 409, 'conflict');
-// createError('Unprocessable', 422, 'unprocessable');
-// createError('GeneralError', 500, 'error');
-// createError('NotImplemented', 501, 'not-implemented');
-// createError('Unavailable', 503, 'unavailable');
-
+export default errors;
 export { errors, types };
