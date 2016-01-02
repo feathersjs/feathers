06e83c910c00cf6340bdeab2c0d32f8e721078c9
David Luecke <daff@neyeon.com>
2016-01-02 07:03:35 -0800

    SocketIO provider migrated to separate repository

--- packages/primus/src/index.js
+++ packages/primus/src/index.js
@@ -1,9 +1,48 @@
 import makeDebug from 'debug';
+import Proto from 'uberproto';
+import { socket as commons } from 'feathers-commons';
+import Primus from 'primus';
+import Emitter from 'primus-emitter';
 
 const debug = makeDebug('feathers-primus');
 
-export default function() {
+export default function(config, configurer) {
   return function() {
-    debug('Initializing feathers-primus plugin');
+    const app = this;
+
+    // Monkey patch app.setup(server)
+    Proto.mixin({
+      service: commons.service,
+
+      setup(server) {
+        debug('Setting up Primus');
+
+        const primus = this.primus = new Primus(server, config);
+
+        primus.use('emitter', Emitter);
+
+        if (typeof configurer === 'function') {
+          debug('Calling Primus configuration function');
+          configurer.call(this, primus);
+        }
+
+        const result = this._super.apply(this, arguments);
+
+        commons.setup.call(this, {
+          method: 'send',
+          connection() {
+            return primus;
+          },
+          clients() {
+            return primus;
+          },
+          params(spark) {
+            return spark.request.feathers;
+          }
+        });
+
+        return result;
+      }
+    }, app);
   };
 }
