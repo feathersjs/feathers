6c66c2d475745fc3ecaf8f2f2e5855799414541b
David Luecke <daff@neyeon.com>
2016-01-09 21:58:42 -0800

    Adding socket client service and shared client tests

--- packages/commons/src/utils.js
+++ packages/commons/src/utils.js
@@ -1,3 +1,21 @@
+export const methods = [
+  'find',
+  'get',
+  'create',
+  'update',
+  'patch',
+  'remove'
+];
+
+export const eventMappings = {
+  create: 'created',
+  update: 'updated',
+  patch: 'patched',
+  remove: 'removed'
+};
+
+export const events = Object.keys(eventMappings).map(method => eventMappings[method]);
+
 export function stripSlashes(name) {
   return name.replace(/^(\/*)|(\/*)$/g, '');
 }
