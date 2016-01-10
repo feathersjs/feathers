1aa853771a0b3486432a149a06006c7cc4e248d0
David Luecke <daff@neyeon.com>
2016-01-09 22:59:33 -0800

    feathers-primus/client service and tests

---
+++ packages/primus/test/client.test.js
@@ -0,0 +1,34 @@
+import assert from 'assert';
+import feathers from 'feathers/client';
+import baseTests from 'feathers-commons/lib/test/client';
+
+import server from './server';
+import primus from '../client';
+
+describe('feathers-primus/client', function() {
+  const app = feathers().configure(primus({}));
+  const service = app.service('todos');
+
+  before(function(done) {
+    this.server = server(primus => {
+      service.connection = this.socket = new primus.Socket('http://localhost:12012');
+    }).listen(12012);
+    this.server.on('listening', () => done());
+  });
+
+  after(function(done) {
+    this.socket.socket.close();
+    this.server.close(done);
+  });
+
+  it('throws an error with no connection', () => {
+    try {
+      feathers().configure(primus());
+      assert.ok(false);
+    } catch(e) {
+      assert.equal(e.message, 'Primus connection needs to be provided');
+    }
+  });
+
+  baseTests(service);
+});
---
+++ packages/primus/test/server.js
@@ -0,0 +1,39 @@
+import feathers from 'feathers';
+import primus from '../src';
+import memory from 'feathers-memory';
+
+Object.defineProperty(Error.prototype, 'toJSON', {
+  value: function () {
+    var alt = {};
+
+    Object.getOwnPropertyNames(this).forEach(function (key) {
+      alt[key] = this[key];
+    }, this);
+
+    return alt;
+  },
+  configurable: true
+});
+
+export default function(cb) {
+  // Create an in-memory CRUD service for our Todos
+  var todoService = memory().extend({
+    get: function(id, params, callback) {
+      if(params.query.error) {
+        return callback(new Error('Something went wrong'));
+      }
+
+      return this._super(id, params).then(data => Object.assign({ query: params.query }, data));
+    }
+  });
+
+  var app = feathers()
+    .configure(primus({
+      transformer: 'websockets'
+    }, cb))
+    .use('/todos', todoService);
+
+  app.service('todos').create({ text: 'some todo', complete: false }, {}, function() {});
+
+  return app;
+}
