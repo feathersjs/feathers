dd220344a09bc87310684676c41487be6e95f5bb
David Luecke <daff@neyeon.com>
2016-01-17 14:16:52 -0800

    Moving shared socket functionality to another repository

--- packages/commons/test/module.test.js
+++ packages/commons/test/module.test.js
@@ -4,7 +4,6 @@ describe('build', () => {
   it('it build and exported', () => {
     let commons = require('../lib/commons');
     assert.equal(typeof commons, 'object');
-    assert.equal(typeof commons.socket, 'object');
     assert.equal(typeof commons.getArguments, 'function');
     assert.equal(typeof commons.stripSlashes, 'function');
     assert.equal(typeof commons.hooks, 'object');
