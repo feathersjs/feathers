eb1681cf4d1e9360926f893c909cc1dc385ae483
David Luecke <daff@neyeon.com>
2016-01-17 09:34:26 -0800

    Refactoring common socket setup into a service mixin

---
+++ packages/commons/src/sockets/methods.js
@@ -0,0 +1,42 @@
+import getArguments from '../arguments';
+
+export function errorObject(e) {
+  let result = {};
+  Object.getOwnPropertyNames(e).forEach(key => result[key] = e[key]);
+  return result;
+}
+
+// The position of the params parameters for a service method so that we can extend them
+// default is 1
+export const paramsPositions = {
+  find: 0,
+  update: 2,
+  patch: 2
+};
+
+// Set up all method handlers for a service and socket.
+export function setupMethodHandlers(info, socket, path, service) {
+  this.methods.forEach(function (method) {
+    if (typeof service[method] !== 'function') {
+      return;
+    }
+
+    let name = `${path}::${method}`;
+    let params = info.params(socket);
+    let position = typeof paramsPositions[method] !== 'undefined' ?
+      paramsPositions[method] : 1;
+
+    socket.on(name, function () {
+      try {
+        let args = getArguments(method, arguments);
+        args[position] = Object.assign({ query: args[position] }, params);
+        service[method].apply(service, args);
+      } catch(e) {
+        let callback = arguments[arguments.length - 1];
+        if(typeof callback === 'function') {
+          callback(errorObject(e));
+        }
+      }
+    });
+  });
+}
