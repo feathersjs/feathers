9bfe9ac5e5e1c79475c595a268f21370f36cb05f
Eric Kryski <e.kryski@gmail.com>
2016-02-12 13:54:02 -0700

    removing code we don't need from sub-app example

--- packages/authentication/examples/sub-apps/index.js
+++ packages/authentication/examples/sub-apps/index.js
@@ -1,19 +1,8 @@
 var feathers = require('feathers');
-var rest = require('feathers-rest');
-var socketio = require('feathers-socketio');
-var hooks = require('feathers-hooks');
-var bodyParser = require('body-parser');
 var api = require('./api');
 
 // Initialize the application
 var app = feathers()
-  .configure(rest())
-  .configure(socketio())
-  .configure(hooks())
-  // Needed for parsing bodies (login)
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }))
-  // Configure feathers-authentication
   // Initialize our API sub app
   .use('/api', api)
   .use('/', feathers.static(__dirname + '/public'));
