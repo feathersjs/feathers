ba9c21f7465d524116a7383cda8e9642f6d525bf
Eric Kryski <e.kryski@gmail.com>
2016-02-14 20:34:40 -0700

    Merge pull request #35 from feathersjs/error-handling
    updating to new feathers-errors error handler

--- packages/generator-feathers/generators/app/templates/middleware.js
+++ packages/generator-feathers/generators/app/templates/middleware.js
@@ -1,24 +1,15 @@
 import path from 'path';
 import notFound from './not-found-handler';
-import error from './error-handler';
+import { handler as error } from 'feathers-errors';
 import logger from './logger';
 
 export default function() {
   const app = this;
-
-  // Register our error routes
-  app.get('/404', function(req, res, next){
-    res.sendFile(path.join(app.get('public'), 'error.html'));
-  });
-
-  app.get('/error', function(req, res, next){
-    res.sendFile(path.join(app.get('public'), 'error.html'));
-  });
 
   // Add your custom middleware here. Remember, that
   // just like Express the order matters, so error
   // handling middleware should go last.
   app.use(notFound())
     .use(logger(app))
-    .use(error(app));
+    .use(error());
 }
