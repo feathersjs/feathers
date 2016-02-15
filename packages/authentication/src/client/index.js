11d3b1991866d38ef17489421e84c6020993c3d7
Eric Kryski <e.kryski@gmail.com>
2016-02-15 15:02:38 -0700

    Merge pull request #65 from fastlorenzo/master
    Fixing client query

--- packages/authentication/src/client/index.js
+++ packages/authentication/src/client/index.js
@@ -19,7 +19,7 @@ export default function(options = {}) {
       if (!options.type) {
         throw new Error('You need to provide a `type` attribute when calling app.authenticate()');
       }
-      
+
       let endPoint;
 
       if (options.type === 'local') {
@@ -33,7 +33,7 @@ export default function(options = {}) {
 
       return new Promise(function(resolve, reject){
         // TODO (EK): Handle OAuth logins
-        
+
         // If we are using a REST client
         if (app.rest) {
           return app.service(endPoint).create(options).then(response => {
@@ -46,7 +46,7 @@ export default function(options = {}) {
 
         if (app.io || app.primus) {
           const transport = app.io ? 'io' : 'primus';
-          
+
           app[transport].on('unauthorized', function(error) {
             // console.error('Unauthorized', error);
             return reject(error);
@@ -67,7 +67,7 @@ export default function(options = {}) {
           if (!app.io.connected) {
             throw new Error('Socket not connected');
           }
-          
+
           app.io.on('disconnect', function(error) {
             // console.error('Socket disconnected', error);
             return reject(error);
@@ -87,7 +87,7 @@ export default function(options = {}) {
             console.error('Socket disconnected', error);
             return reject(error);
           });
-          
+
           app.primus.send('authenticate', options);
         }
       });
@@ -108,7 +108,7 @@ export default function(options = {}) {
     app.mixins.push(function(service) {
       service.before(hooks.populateParams());
     });
-    
+
     // Set up hook that adds authorization header
     if (app.rest) {
       app.mixins.push(function(service) {
@@ -116,11 +116,5 @@ export default function(options = {}) {
       });
     }
 
-    // Set up hook that adds adds token to data sent to server over sockets
-    if (app.io || app.primus) {
-      app.mixins.push(function(service) {
-        service.before(hooks.populateSocketParams());
-      });
-    }
   };
 }
