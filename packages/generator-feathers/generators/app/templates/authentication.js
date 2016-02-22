9e29146a1cfcb1e5721f22328a5c73c24ed5367f
Eric Kryski <e.kryski@gmail.com>
2016-02-21 23:59:17 -0700

    Merge pull request #49 from feathersjs/oauth
    Oauth. Closes #2. Closes https://github.com/feathersjs/feathers/issues/229.

---
+++ packages/generator-feathers/generators/app/templates/authentication.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const authentication = require('feathers-authentication');
+<% for (var i = 0; i < authentication.length; i++) { %>
+const <%= S(authentication[i].name).capitalize().s %>Strategy = require('<%= authentication[i].strategy %>').Strategy;<% if (authentication[i].tokenStrategy) { %>
+const <%= S(authentication[i].name).capitalize().s %>TokenStrategy = require('<%= authentication[i].tokenStrategy %>').Strategy;<% }} %>
+
+module.exports = function() {
+  const app = this;
+
+  let config = app.get('auth');
+  <% for (var i = 0; i < authentication.length; i++) { %>
+  config.<%= authentication[i].name %>.strategy = <%= S(authentication[i].name).capitalize().s %>Strategy;<% if (authentication[i].tokenStrategy) { %>
+  config.<%= authentication[i].name %>.tokenStrategy = <%= S(authentication[i].name).capitalize().s %>TokenStrategy;<% }} %>
+
+  app.set('auth', config);
+  app.configure(authentication(config));
+}
