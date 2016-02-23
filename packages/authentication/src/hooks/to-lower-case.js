8c3670e1c26504c584d5762f7bfda816cb9f08bf
Eric Kryski <e.kryski@gmail.com>
2016-02-23 08:44:52 -0700

    Merge pull request #75 from enten/master
    Fix toLowerCase hook

--- packages/authentication/src/hooks/to-lower-case.js
+++ packages/authentication/src/hooks/to-lower-case.js
@@ -4,15 +4,15 @@
  * looks for the key on the data object for before hooks and the result object for
  * the after hooks.
  */
-export default function toLowercase(options = {}) {
+export default function toLowerCase(options = {}) {
   const fieldName = options.fieldName;
-  
+
   if (!fieldName) {
     throw new Error('You must provide the name of the field to use in the toLowerCase hook.');
   }
 
   function convert(obj){
-    if (obj[fieldName] && obj[fieldName].toLowercase) {
+    if (obj[fieldName] && obj[fieldName].toLowerCase) {
       obj[fieldName] = obj[fieldName].toLowerCase();
     }
   }
