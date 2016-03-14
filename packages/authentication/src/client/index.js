9106bd9e36ce128bb40a3809e1f8fc289ea73d83
Eric Kryski <e.kryski@gmail.com>
2016-03-14 15:35:25 -0600

    Merge pull request #101 from feathersjs/client-auth-polish
    Finalizing client side authentication module

--- packages/authentication/src/client/index.js
+++ packages/authentication/src/client/index.js
@@ -1,137 +1,92 @@
 import * as hooks from './hooks';
+import { connected, authenticateSocket, getJWT, getStorage } from './utils';
 
 const defaults = {
-  storage: '/storage',
+  tokenKey: 'feathers-jwt',
   localEndpoint: '/auth/local',
   tokenEndpoint: '/auth/token'
 };
 
 export default function(opts = {}) {
-  const authOptions = Object.assign({}, defaults, opts);
+  const config = Object.assign({}, defaults, opts);
 
   return function() {
     const app = this;
-    const storage = () => app.service(authOptions.storage);
-    
-    if (!storage) {
-      throw new Error(`You need register a local store before you can use feathers-authentication. Did you call app.use('storage', localstorage())`);
+
+    if(!app.get('storage')) {
+      app.set('storage', getStorage(config.storage));
     }
 
-    const handleResponse = function (response) {
-      return storage().create([{
-        id: 'token',
-        value: response.token
-      }, {
-        id: 'user',
-        value: response.data
-      }]).then(() => response);
-    };
+    app.authenticate = function(options = {}) {
+      const storage = this.get('storage');
+      let getOptions = Promise.resolve(options);
 
-    app.authenticate = function(options) {
+      // If no type was given let's try to authenticate with a stored JWT
       if (!options.type) {
-        throw new Error('You need to provide a `type` attribute when calling app.authenticate()');
+        getOptions = getJWT(config.tokenKey, this.get('storage')).then(token => {
+          if(!token) {
+            return Promise.reject(new Error(`Could not find stored JWT and no authentication type was given`));
+          }
+
+          return { type: 'token', token };
+        });
       }
 
-      let endPoint;
+      const handleResponse = function (response) {
+        app.set('token', response.token);
+        app.set('user', response.data);
 
-      if (options.type === 'local') {
-        endPoint = authOptions.localEndpoint;
-      } else if (options.type === 'token') {
-        endPoint = authOptions.tokenEndpoint;
-      } else {
-        throw new Error(`Unsupported authentication 'type': ${options.type}`);
-      }
+        return Promise.resolve(storage.setItem(config.tokenKey, response.token))
+          .then(() => response);
+      };
 
-      return new Promise(function(resolve, reject) {
-        // TODO (EK): Handle OAuth logins
-        // If we are using a REST client
-        if (app.rest) {
-          return app.service(endPoint).create(options).then(handleResponse);
-        }
-        
-        if (app.io || app.primus) {
-          const socket = app.io || app.primus;
-          const handleUnauthorized = function(error) {
-            // Unleak event handlers
-            this.off('disconnect', reject);
-            this.off('close', reject);
-            
-            reject(error);
-          };
-          const handleAuthenticated = function(response) {
-            // We need to bind and unbind the event handlers that didn't run
-            // so that they don't leak around
-            this.off('unauthorized', handleUnauthorized);
-            this.off('disconnect', reject);
-            this.off('close', reject);
-            
-            handleResponse(response).then(reponse => resolve(reponse)).catch(error => {
-              throw error;
-            });
-          };
-          
-          // Also, binding to events that aren't fired (like `close`)
-          // for Socket.io doesn't hurt if we unbind once we're done
-          socket.once('disconnect', reject);
-          socket.once('close', reject);
-          socket.once('unauthorized', handleUnauthorized);
-          socket.once('authenticated', handleAuthenticated);
+      return getOptions.then(options => {
+        let endPoint;
+
+        if (options.type === 'local') {
+          endPoint = config.localEndpoint;
+        } else if (options.type === 'token') {
+          endPoint = config.tokenEndpoint;
+        } else {
+          throw new Error(`Unsupported authentication 'type': ${options.type}`);
         }
 
-        // If we are using socket.io
-        if (app.io) {
-          const socket = app.io;
-          
-          // If we aren't already connected then throw an error
-          if (!socket.connected) {
-            throw new Error('Socket not connected');
+        return connected(app).then(socket => {
+          // TODO (EK): Handle OAuth logins
+          // If we are using a REST client
+          if (app.rest) {
+            return app.service(endPoint).create(options).then(handleResponse);
           }
-          
-          socket.emit('authenticate', options);
-        }
 
-        // If we are using primus
-        if (app.primus) {
-          const socket = app.primus;
-          
-          // If we aren't already connected then throw an error
-          if (socket.readyState !== 3) {
-            throw new Error('Socket not connected');
-          }
-          
-          socket.send('authenticate', options);
-        }
+          const method = app.io ? 'emit' : 'send';
+
+          return authenticateSocket(options, socket, method).then(handleResponse);
+        });
       });
     };
 
-    app.user = function() {
-      return storage().get('user').then(data => data.value);
-    };
-    
-    app.token = function() {
-      return storage().get('token').then(data => data.value);
-    };
-
     app.logout = function() {
-      return storage().remove(null, { id: { $in: ['user', 'token' ] } });
+      app.set('user', null);
+      app.set('token', null);
+      
+      // TODO (EK): invalidate token with server
+      return Promise.resolve(app.get('storage').setItem(config.tokenKey, null));
     };
 
     // Set up hook that adds adds token and user to params so that
     // it they can be accessed by client side hooks and services
     app.mixins.push(function(service) {
-      if (!service.before || !service.after) {
-        throw new Error(`It looks like feathers-hooks isn't configured. It is required before you configure feathers-authentication.`);
+      if (typeof service.before !== 'function' || typeof service.after !== 'function') {
+        throw new Error(`It looks like feathers-hooks isn't configured. It is required before running feathers-authentication.`);
       }
-      service.before(hooks.populateParams(authOptions));
+
+      service.before(hooks.populateParams(config));
     });
-    
+
     // Set up hook that adds authorization header for REST provider
     if (app.rest) {
       app.mixins.push(function(service) {
-        if (!service.before || !service.after) {
-          throw new Error(`It looks like feathers-hooks isn't configured. It is required before you configure feathers-authentication.`);
-        }
-        service.before(hooks.populateHeader(authOptions));
+        service.before(hooks.populateHeader(config));
       });
     }
   };
--- packages/authentication/test/client/index.test.js
+++ packages/authentication/test/client/index.test.js
@@ -1,73 +1,215 @@
 import assert from 'assert';
 import io from 'socket.io-client';
-import memory from 'feathers-memory';
 import hooks from 'feathers-hooks';
 import feathers from 'feathers/client';
 import socketio from 'feathers-socketio/client';
+import primus from 'feathers-primus/client';
+import rest from 'feathers-rest/client';
+import localstorage from 'localstorage-memory';
+import request from 'request';
+import Primus from 'primus';
+import Emitter from 'primus-emitter';
 
 import authentication from '../../src/client';
 import createApplication from '../test-server';
 
-describe('Client side authentication', () => {
-  const socket = io('http://localhost:8888');
-  const email = 'test@feathersjs.com';
-  const password = 'test';
-  const settings = {
-    idField: 'id',
-    token: {
-      secret: 'feathers-rocks'
-    }
-  };
-  
-  let app, server;
-  
-  before(done => {
-    createApplication(settings, email, password, true, (err, obj) => {
-      app = obj.app;
-      server = obj.server;
-      
-      setTimeout(done, 10);
-    });
-  });
-  
-  after(done => {
-    socket.once('disconnect', () => server.close(done));
-    socket.disconnect();
-  });
-  
-  it('adds .authenticate, .user, .token and .logout', () => {
-    const app = feathers().configure(authentication());
-    
-    assert.equal(typeof app.authenticate, 'function');
-    assert.equal(typeof app.user, 'function');
-    assert.equal(typeof app.token, 'function');
-    assert.equal(typeof app.logout, 'function');
-  });
-  
-  it('authenticates via Socket.io', done => {
-    const app = feathers()
-      .configure(socketio(socket))
-      .configure(hooks())
-      .configure(authentication())
-      .use('/storage', memory());
-    
-    socket.on('connect', () => {
-      app.authenticate({
+const email = 'test@feathersjs.com';
+const password = 'test';
+const settings = {
+  idField: 'id',
+  token: {
+    secret: 'feathers-rocks'
+  }
+};
+const setupTests = initApp => {
+  let app;
+
+  beforeEach(() => app = initApp());
+
+  it('local username password authentication', done => {
+    app.authenticate({
         type: 'local',
         email, password
       }).then(response => {
         assert.ok(response.token);
         assert.ok(response.data);
-        
-        app.user()
-          .then(user => {
-            assert.deepEqual(user, response.data);
-            
-            return app.token();
-          })
-          .then(token => assert.deepEqual(token, response.token))
-          .then(done);
+
+        assert.deepEqual(app.get('token'), response.token);
+        assert.deepEqual(app.get('user'), response.data);
+
+        done();
       }).catch(done);
+  });
+
+  it('local username password authentication and access to protected service', done => {
+    app.authenticate({
+        type: 'local',
+        email, password
+      }).then(response => {
+        assert.ok(response.token);
+        assert.ok(response.data);
+
+        return app.service('messages').create({ text: 'auth test message' })
+          .then(msg => {
+            assert.ok(typeof msg.id !== 'undefined');
+            done();
+          });
+      }).catch(done);
+  });
+
+  it('local authentication with wrong credentials fails', done => {
+    app.authenticate({
+        type: 'local',
+        email,
+        password: 'this is wrong'
+      })
+      .then(() => done(new Error('Should never get here')))
+      .catch(error => {
+        assert.equal(error.name, 'NotAuthenticated');
+        assert.equal(error.code, 401);
+        done();
+      });
+  });
+
+  it('authentication with no options and no stored token fails', done => {
+    app.authenticate()
+      .then(() => done(new Error('Should never get here')))
+      .catch(error => {
+        assert.equal(error.message, 'Could not find stored JWT and no authentication type was given');
+        done();
+      });
+  });
+
+  it('uses localStorage compatible stores', done => {
+    const oldStorage = app.get('storage');
+    app.set('storage', localstorage);
+
+    app.authenticate({
+        type: 'local',
+        email, password
+      }).then(response => {
+        assert.equal(response.token, localstorage.getItem('feathers-jwt'));
+        app.set('storage', oldStorage);
+        done();
+      });
+  });
+
+  it('token is stored and re-authentication with stored token works', done => {
+    app.authenticate({
+        type: 'local',
+        email, password
+      }).then(response => {
+        assert.ok(response.token);
+        assert.ok(response.data);
+
+        return app.authenticate().then(response => {
+          assert.deepEqual(app.get('token'), response.token);
+          assert.deepEqual(app.get('user'), response.data);
+        }).then(done);
+      }).catch(done);
+  });
+
+  it.skip('.logout works, does not grant access to protected service', done => {
+    app.authenticate({
+        type: 'local',
+        email, password
+      }).then(response => {
+        assert.ok(response.token);
+        assert.ok(response.data);
+
+        app.logout().then(() => {
+          app.service('messages').create({ text: 'auth test message' })
+            .then(msg => console.log('!!!', msg))
+            .catch(error => console.log(error));
+        });
+      }).catch(done);
+  });
+};
+
+describe('Client side authentication', () => {
+  it('adds .authenticate, and .logout', () => {
+    const app = feathers().configure(authentication());
+
+    assert.equal(typeof app.authenticate, 'function');
+    assert.equal(typeof app.logout, 'function');
+  });
+
+  describe('REST client authentication', () => {
+    const connection = rest('http://localhost:8888').request(request);
+    let server;
+
+    before(done => {
+      createApplication(settings, email, password, true, (err, obj) => {
+        server = obj.server;
+
+        setTimeout(done, 10);
+      });
+    });
+
+    after(done => server.close(done));
+
+    setupTests(() => {
+      return feathers()
+        .configure(connection)
+        .configure(hooks())
+        .configure(authentication());
+    });
+  });
+
+  describe('Socket.io client authentication', () => {
+    let socket, server;
+
+    before(done => {
+      createApplication(settings, email, password, true, (err, obj) => {
+        server = obj.server;
+        socket = io('http://localhost:8888');
+
+        setTimeout(done, 10);
+      });
+    });
+
+    after(done => {
+      socket.once('disconnect', () => server.close(done));
+      socket.disconnect();
+    });
+
+    setupTests(() => {
+      return feathers()
+        .configure(socketio(socket))
+        .configure(hooks())
+        .configure(authentication());
+    });
+  });
+
+  describe('Primus client authentication', () => {
+    let socket, server;
+
+    before(done => {
+      createApplication(settings, email, password, false, (err, obj) => {
+        const Socket = Primus.createSocket({
+          transformer: 'websockets',
+          plugin: {
+            'emitter': Emitter
+          }
+        });
+
+        server = obj.server;
+        socket = new Socket('http://localhost:8888');
+
+        setTimeout(done, 10);
+      });
+    });
+
+    after(done => {
+      socket.socket.close();
+      server.close(done);
+    });
+
+    setupTests(() => {
+      return feathers()
+        .configure(primus(socket))
+        .configure(hooks())
+        .configure(authentication());
     });
   });
 });
