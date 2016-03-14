d615898096921fef7dbc39d639687b187858793d
Eric Kryski <e.kryski@gmail.com>
2016-03-14 17:38:53 -0600

    minor example fixes

--- packages/authentication/example/client.js
+++ packages/authentication/example/client.js
@@ -1,16 +1,17 @@
 // This is what a NodeJS client looks like
 const io = require('socket.io-client');
-const feathers = require('feathers/client');
+const feathers = require('feathers');
 const socketio = require('feathers-socketio/client');
 const hooks = require('feathers-hooks');
 const authentication = require('../lib/client');
+const localstorage = require('localstorage-memory');
 const host = 'http://localhost:3030';
 const socket = io(host);
 
 const app = feathers()
   .configure(socketio(socket))
   .configure(hooks())
-  .configure(authentication());
+  .configure(authentication({ storage: localstorage }));
 
 app.authenticate({
   type: 'local',
