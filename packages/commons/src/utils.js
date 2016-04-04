8855827f3a3892aa6c60f9ae6b9dad7be6cf5176
David Luecke <daff@neyeon.com>
2016-04-03 21:34:37 -0700

    Adding functionality and tests for shared query and list handling

--- packages/commons/src/utils.js
+++ packages/commons/src/utils.js
@@ -9,3 +9,101 @@ export function each(obj, callback) {
     Object.keys(obj).forEach(key => callback(obj[key], key));
   }
 }
+
+export const _ = {
+  some(value, callback) {
+    return Object.keys(value)
+      .map(key => [ value[key], key ])
+      .some(current => callback(... current));
+  },
+  every(value, callback) {
+    return Object.keys(value)
+      .map(key => [ value[key], key ])
+      .every(current => callback(... current));
+  },
+  isMatch(obj, item) {
+    return Object.keys(item).every(key => obj[key] === item[key]);
+  },
+  omit(obj, ...keys) {
+    const result = Object.assign({}, obj);
+    for(let key of keys) {
+      delete result[key];
+    }
+    return result;
+  }
+};
+
+export const specialFilters = {
+  $in(key, ins) {
+    return current => ins.indexOf(current[key]) !== -1;
+  },
+
+  $nin(key, nins) {
+    return current => nins.indexOf(current[key]) === -1;
+  },
+
+  $lt(key, value) {
+    return current => current[key] < value;
+  },
+
+  $lte(key, value) {
+    return current => current[key] <= value;
+  },
+
+  $gt(key, value) {
+    return current => current[key] > value;
+  },
+
+  $gte(key, value) {
+    return current => current[key] >= value;
+  },
+
+  $ne(key, value) {
+    return current => current[key] !== value;
+  }
+};
+
+export function matcher(originalQuery) {
+  const query = _.omit(originalQuery, '$limit', '$skip', '$sort');
+  
+  return function(item) {
+    if(query.$or && _.some(query.$or, or => _.isMatch(item, or))) {
+      return true;
+    }
+
+    return _.every(query, (value, key) => {
+      if(typeof value === 'object') {
+        return _.every(value, (target, filterType) => {
+          if(specialFilters[filterType]) {
+            const filter = specialFilters[filterType](key, target);
+            return filter(item);
+          }
+
+          return false;
+        });
+      } else if(typeof item[key] !== 'undefined') {
+        return item[key] === query[key];
+      }
+
+      return false;
+    });
+  };
+}
+
+export function sorter($sort) {
+  return function(first, second) {
+    let comparator = 0;
+    each($sort, (modifier, key) => {
+      modifier = parseInt(modifier, 10);
+
+      if(first[key] < second[key]) {
+        comparator -= 1 * modifier;
+      }
+
+      if(first[key] > second[key]) {
+        comparator += 1 * modifier;
+      }
+    });
+    return comparator;
+  };
+}
---
+++ packages/commons/test/utils.test.js
@@ -0,0 +1,156 @@
+import assert from 'assert';
+import * as utils from '../src/utils';
+
+describe('feathers-commons utils', () => {
+  it('stripSlashes', () => {
+    assert.equal(utils.stripSlashes('some/thing'), 'some/thing');
+    assert.equal(utils.stripSlashes('/some/thing'), 'some/thing');
+    assert.equal(utils.stripSlashes('some/thing/'), 'some/thing');
+    assert.equal(utils.stripSlashes('/some/thing/'), 'some/thing');
+    assert.equal(utils.stripSlashes('//some/thing/'), 'some/thing');
+    assert.equal(utils.stripSlashes('//some//thing////'), 'some//thing');
+  });
+
+  describe('specialFilters', () => {
+    const filters = utils.specialFilters;
+
+    it('$in', () => {
+      const fn = filters.$in('test', ['a', 'b']);
+
+      assert.ok(fn({ test: 'a' }));
+      assert.ok(!fn({ test: 'c' }));
+    });
+
+    it('$nin', () => {
+      const fn = filters.$nin('test', ['a', 'b']);
+
+      assert.ok(!fn({ test: 'a' }));
+      assert.ok(fn({ test: 'c' }));
+    });
+
+    it('$lt', () => {
+      const fn = filters.$lt('age', 25);
+
+      assert.ok(fn({ age: 24 }));
+      assert.ok(!fn({ age: 25 }));
+      assert.ok(!fn({ age: 26 }));
+    });
+
+    it('$lte', () => {
+      const fn = filters.$lte('age', 25);
+
+      assert.ok(fn({ age: 24 }));
+      assert.ok(fn({ age: 25 }));
+      assert.ok(!fn({ age: 26 }));
+    });
+
+    it('$gt', () => {
+      const fn = filters.$gt('age', 25);
+
+      assert.ok(!fn({ age: 24 }));
+      assert.ok(!fn({ age: 25 }));
+      assert.ok(fn({ age: 26 }));
+    });
+
+    it('$gte', () => {
+      const fn = filters.$gte('age', 25);
+
+      assert.ok(!fn({ age: 24 }));
+      assert.ok(fn({ age: 25 }));
+      assert.ok(fn({ age: 26 }));
+    });
+
+    it('$ne', () => {
+      const fn = filters.$ne('test', 'me');
+
+      assert.ok(fn({ test: 'you' }));
+      assert.ok(!fn({ test: 'me' }));
+    });
+  });
+
+  describe('sorter', () => {
+    it('simple sorter', () => {
+      const array = [{
+        name: 'David'
+      }, {
+        name: 'Eric'
+      }];
+
+      const sorter = utils.sorter({
+        name: -1
+      });
+
+      assert.deepEqual(array.sort(sorter), [{
+        name: 'Eric'
+      }, {
+        name: 'David'
+      }]);
+    });
+
+    it('two property sorter', () => {
+      const array = [{
+        name: 'David',
+        counter: 0
+      }, {
+        name: 'Eric',
+        counter: 1
+      }, {
+        name: 'David',
+        counter: 1
+      }, {
+        name: 'Eric',
+        counter: 0
+      }];
+
+      const sorter = utils.sorter({
+        name: -1,
+        counter: 1
+      });
+
+      assert.deepEqual(array.sort(sorter), [
+        { name: 'Eric', counter: 0 },
+        { name: 'David', counter: 0 },
+        { name: 'Eric', counter: 1 },
+        { name: 'David', counter: 1 }
+      ]);
+    });
+  });
+
+  describe('matcher', () => {
+    it('simple match', () => {
+      const matches = utils.matcher({ name: 'Eric' });
+
+      assert.ok(matches({ name: 'Eric' }));
+      assert.ok(!matches({ name: 'David' }));
+    });
+
+    it('$or match', () => {
+      const matches = utils.matcher({ $or: [{ name: 'Eric' }, { name: 'Marshall' }] });
+
+      assert.ok(matches({ name: 'Eric' }));
+      assert.ok(matches({ name: 'Marshall' }));
+      assert.ok(!matches({ name: 'David' }));
+    });
+
+    it('special filter matches', () => {
+      const matches = utils.matcher({
+        counter: { $gt: 10, $lte: 19 },
+        name: { $in: ['Eric', 'Marshall'] }
+      });
+
+      assert.ok(matches({ name: 'Eric', counter: 12 }));
+      assert.ok(!matches({ name: 'Eric', counter: 10 }));
+      assert.ok(matches({ name: 'Marshall', counter: 19 }));
+    });
+
+    it('special filter and simple matches', () => {
+      const matches = utils.matcher({
+        counter: 0,
+        name: { $in: ['Eric', 'Marshall'] }
+      });
+
+      assert.ok(!matches({ name: 'Eric', counter: 1 }));
+      assert.ok(matches({ name: 'Marshall', counter: 0 }));
+    });
+  });
+});
