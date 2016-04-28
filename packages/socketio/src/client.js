aae044f255f24723ccd5d2767efb3c5be7d4df3c
David Luecke <daff@neyeon.com>
2016-04-27 23:04:11 -0700

    Add timeout option

--- packages/socketio/src/client.js
+++ packages/socketio/src/client.js
@@ -1,25 +1,31 @@
 import Service from 'feathers-socket-commons/client';
 
-export default function(connection) {
+export default function(connection, options) {
   if(!connection) {
     throw new Error('Socket.io connection needs to be provided');
   }
-  
+
   const defaultService = function(name) {
-    return new Service({ name, connection, method: 'emit' });
+    const settings = Object.assign({}, options, {
+      name,
+      connection,
+      method: 'emit'
+    });
+
+    return new Service(settings);
   };
 
   const initialize = function() {
     if(typeof this.defaultService === 'function') {
       throw new Error('Only one default client provider can be configured');
     }
-    
+
     this.io = connection;
     this.defaultService = defaultService;
   };
-  
+
   initialize.Service = Service;
   initialize.service = defaultService;
-  
+
   return initialize;
 }
