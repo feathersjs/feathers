590e3f426ae4eb631ace091799b2b068e3ef3d11
David Luecke <daff@neyeon.com>
2016-04-27 23:36:02 -0700

    Implement options for setting client timeout (#16)

--- packages/primus/src/client.js
+++ packages/primus/src/client.js
@@ -1,25 +1,29 @@
 import Service from 'feathers-socket-commons/client';
 
-export default function(connection) {
+export default function(connection, options) {
   if(!connection) {
     throw new Error('Primus connection needs to be provided');
   }
-  
+
   const defaultService = function(name) {
-    return new Service({ name, connection, method: 'send' });
+    return new Service(Object.assign({}, options, {
+      name,
+      connection,
+      method: 'send'
+    }));
   };
 
   const initialize = function() {
     if(typeof this.defaultService === 'function') {
       throw new Error('Only one default client provider can be configured');
     }
-    
+
     this.primus = connection;
     this.defaultService = defaultService;
   };
-  
+
   initialize.Service = Service;
   initialize.service = defaultService;
-  
+
   return initialize;
 }
