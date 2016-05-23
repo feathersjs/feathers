feb5ce881d6d954a7fd6af701a2e0f15dfadfe6e
David Luecke <daff@neyeon.com>
2016-05-22 23:32:19 -0700

    Adding tests and fix for websockets in sub-apps

--- packages/primus/src/index.js
+++ packages/primus/src/index.js
@@ -10,21 +10,25 @@ export default function(config, configurer) {
   return function() {
     const app = this;
 
-    app.configure(socket);
+    app.configure(socket('primus'));
 
     // Monkey patch app.setup(server)
     Proto.mixin({
       setup(server) {
         debug('Setting up Primus');
 
-        const primus = this.primus = new Primus(server, config);
+        let primus = this.primus;
 
-        primus.use('emitter', Emitter);
+        if(!primus) {
+          primus = this.primus = new Primus(server, config);
 
-        primus.before('feathers', function(req, res, next) {
-          req.feathers = { provider: 'primus' };
-          next();
-        }, 0);
+          primus.use('emitter', Emitter);
+
+          primus.before('feathers', function(req, res, next) {
+            req.feathers = { provider: 'primus' };
+            next();
+          }, 0);
+        }
 
         if (typeof configurer === 'function') {
           debug('Calling Primus configuration function');
