1695768fb8ee1ca41848b66f152708107ef761cc
David Luecke <daff@neyeon.com>
2016-05-22 18:09:53 -0700

    Finalize sub-app test and functionality

--- packages/socketio/test/sub-app.js
+++ packages/socketio/test/sub-app.js
@@ -2,21 +2,23 @@ import feathers from 'feathers';
 import socketio from '../src';
 import memory from 'feathers-memory';
 
-export default function() {
-  // Create an in-memory CRUD service for our Todos
-  var todoService = memory().extend({
+function todoService() {
+  return memory().extend({
     get: function(id, params, callback) {
       if(params.query.error) {
         return callback(new Error('Something went wrong'));
       }
 
-      return this._super(id, params).then(data => Object.assign({ query: params.query }, data));
+      return this._super(id, params).then(
+        data => Object.assign({ query: params.query }, data)
+      );
     }
   });
-
-  const app = feathers();
-  const v1 = feathers().configure(socketio()).use('/todos', todoService);
-  const v2 = feathers().configure(socketio()).use('/todos', todoService);
+}
+export default function() {
+  const app = feathers().configure(socketio());
+  const v1 = feathers().configure(socketio()).use('/todos', todoService());
+  const v2 = feathers().configure(socketio()).use('/todos', todoService());
 
   app.use('/api/v1', v1);
   app.use('/api/v2', v2);
