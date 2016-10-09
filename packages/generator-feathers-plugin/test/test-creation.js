fdd6feda7536caa189dfa193719343d8b7eca4a0
David Luecke <daff@neyeon.com>
2016-10-09 00:25:35 -0700

    Update to ESLint (#32)

--- packages/generator-feathers-plugin/test/test-creation.js
+++ packages/generator-feathers-plugin/test/test-creation.js
@@ -7,37 +7,36 @@ var fs = require('fs');
 var exec = require('child_process').exec;
 var helpers = require('yeoman-test');
 
-describe('feathers-plugin generator', function () {
-    it('created a plugin with passing tests', function (done) {
-      var tmpDir;
+describe('feathers-plugin generator', function() {
+  it('created a plugin with passing tests', function(done) {
+    var tmpDir;
 
-      helpers.run(path.join(__dirname, '../app'))
-       .inTmpDir(function (dir) {
-         tmpDir = dir;
-       })
-       .withOptions({
-         skipInstall: false
-       })
-       .withPrompts({
-         name: 'feathers-tmp',
-         repository: 'feathersjs/feathers-tmp',
-         description: 'Plugin description here'
-       })
-       .on('end', function () {
-         assert.ok(fs.existsSync(path.join(tmpDir, '.jshintrc')));
-         assert.ok(fs.existsSync(path.join(tmpDir, '.npmignore')));
-         assert.ok(fs.existsSync(path.join(tmpDir, '.travis.yml')));
-         assert.ok(fs.existsSync(path.join(tmpDir, '.editorconfig')));
-         assert.ok(fs.existsSync(path.join(tmpDir, '.babelrc')));
+    helpers.run(path.join(__dirname, '../app'))
+      .inTmpDir(function(dir) {
+        tmpDir = dir;
+      })
+      .withOptions({
+        skipInstall: false
+      })
+      .withPrompts({
+        name: 'feathers-tmp',
+        repository: 'feathersjs/feathers-tmp',
+        description: 'Plugin description here'
+      })
+      .on('end', function() {
+        assert.ok(fs.existsSync(path.join(tmpDir, '.npmignore')));
+        assert.ok(fs.existsSync(path.join(tmpDir, '.travis.yml')));
+        assert.ok(fs.existsSync(path.join(tmpDir, '.editorconfig')));
+        assert.ok(fs.existsSync(path.join(tmpDir, '.babelrc')));
 
-         var child = exec('npm test', {
-           cwd: tmpDir
-         });
+        var child = exec('npm test', {
+          cwd: tmpDir
+        });
 
-         child.on('exit', function (status) {
-           assert.equal(status, 0, 'Got correct exist status');
-           done();
-         });
-       });
-    });
+        child.on('exit', function(status) {
+          assert.equal(status, 0, 'Got correct exist status');
+          done();
+        });
+      });
+  });
 });
