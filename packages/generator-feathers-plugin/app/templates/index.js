b9fece0448e32a49dde2d6f42061410dc3cd2382
Eric Kryski <e.kryski@gmail.com>
2016-10-18 16:21:44 -0600

    fixing lint error by just commenting out feathers-errors usage

--- packages/generator-feathers-plugin/app/templates/index.js
+++ packages/generator-feathers-plugin/app/templates/index.js
@@ -1,14 +1,9 @@
-import errors from 'feathers-errors';
+// import errors from 'feathers-errors';
 import makeDebug from 'debug';
 
 const debug = makeDebug('<%= name %>');
 
 export default function init () {
   debug('Initializing <%= name %> plugin');
-
-  if (error) {
-    throw new errors.GeneralError('There was an error');
-  }
-  
   return '<%= name %>';
 }
