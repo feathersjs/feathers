3422ea51bec8b1c1aefa625abf6fec0f1af35efa
Eric Kryski <e.kryski@gmail.com>
2016-11-13 21:55:14 -0700

    getting tests passing and converting to pulling common defaults from core auth

--- packages/authentication-jwt/src/index.js
+++ packages/authentication-jwt/src/index.js
@@ -1,18 +1,24 @@
 import Debug from 'debug';
 import merge from 'lodash.merge';
 import omit from 'lodash.omit';
-// import hooks from './hooks';
+import pick from 'lodash.pick';
 import DefaultVerifier from './verifier';
 import { Strategy as JWTStrategy, ExtractJwt } from 'passport-jwt';
 
 const debug = Debug('feathers-authentication-jwt');
 const defaults = {
-  name: 'jwt',
-  entity: 'user',
-  service: 'users',
-  passReqToCallback: true,
-  session: false
+  name: 'jwt'
 };
+
+const KEYS = [
+  'secret',
+  'header',
+  'entity',
+  'service',
+  'passReqToCallback',
+  'session',
+  'jwt'
+];
 
 export default function init (options = {}) {
   return function jwtAuth () {
@@ -22,36 +28,27 @@ export default function init (options = {}) {
       throw new Error(`Can not find app.passport. Did you initialize feathers-authentication before feathers-authentication-jwt?`);
     }
 
-    // NOTE (EK): Pull from global auth config to support legacy
-    // auth for an easier transition.
-    const authSettings = app.get('auth') || {};
+    // NOTE (EK): Pull from global auth config to support legacy auth for an easier transition.
+    let jwtSettings = merge(defaults, pick(app.get('auth') || {}, KEYS), omit(options, ['Verifier']));
 
-    let { secret, header } = authSettings;
-
-    if (options.header) {
-      header = options.header;
-    }
-
-    if (typeof header !== 'string') {
+    if (typeof jwtSettings.header !== 'string') {
       throw new Error(`You must provide a 'header' in your authentication configuration or pass one explicitly`);
     }
 
-    let jwtSettings = merge(defaults, authSettings.jwt, omit(options, ['Verifier']));
-    let strategyOptions = merge({
-      secretOrKey: secret,
-      jwtFromRequest: ExtractJwt.fromHeader(header.toLowerCase()),
-      passReqToCallback: defaults.passReqToCallback,
-      session: defaults.session
-    }, authSettings.jwt, omit(options, ['Verifier']));
-    let Verifier = DefaultVerifier;
-
-    if (!strategyOptions.secretOrKey) {
+    if (typeof jwtSettings.secret !== 'string') {
       throw new Error(`You must provide a 'secret' in your authentication configuration or pass one explicitly`);
     }
+
+    let Verifier = DefaultVerifier;
+    let strategyOptions = merge({
+      secretOrKey: jwtSettings.secret,
+      jwtFromRequest: ExtractJwt.fromHeader(jwtSettings.header.toLowerCase())
+    }, jwtSettings.jwt, omit(jwtSettings, ['jwt', 'header', 'secret']));
 
     // Normalize algorithm key
     if (!strategyOptions.algorithms && strategyOptions.algorithm) {
       strategyOptions.algorithms = Array.isArray(strategyOptions.algorithm) ? strategyOptions.algorithm : [strategyOptions.algorithm];
+      delete strategyOptions.algorithm;
     }
 
     // Support passing a custom verifier
@@ -64,10 +61,6 @@ export default function init (options = {}) {
     if (!verifier.verify) {
       throw new Error(`Your verifier must implement a 'verify' function. It should have the same signature as a jwt passport verify callback.`);
     }
-
-    // Set jwt options back on global auth config
-    authSettings.jwt = jwtSettings;
-    app.set('auth', authSettings);
 
     // Register 'jwt' strategy with passport
     debug('Registering jwt authentication strategy with options:', strategyOptions);
