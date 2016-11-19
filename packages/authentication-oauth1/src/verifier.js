91ddeeba9843ffa08e7980df82e85f00a569a45e
Eric Kryski <e.kryski@gmail.com>
2016-11-19 15:39:53 -0700

    OAuth1 is ready to go

---
+++ packages/authentication-oauth1/src/verifier.js
@@ -0,0 +1,113 @@
+import Debug from 'debug';
+import merge from 'lodash.merge';
+
+const debug = Debug('feathers-authentication-oauth1:verify');
+
+class OAuth1Vierifier {
+  constructor (app, options = {}) {
+    this.app = app;
+    this.options = options;
+    this.service = typeof options.service === 'string' ? app.service(options.service) : options.service;
+
+    if (!this.service) {
+      throw new Error(`options.service does not exist.\n\tMake sure you are passing a valid service path or service instance and it is initialized before feathers-authentication-oauth1.`);
+    }
+
+    this._createEntity = this._createEntity.bind(this);
+    this._updateEntity = this._updateEntity.bind(this);
+    this._normalizeResult = this._normalizeResult.bind(this);
+    this.verify = this.verify.bind(this);
+  }
+
+  _normalizeResult (results) {
+    // Paginated services return the array of results in the data attribute.
+    let entities = results.data ? results.data : results;
+    let entity = entities[0];
+
+    // Handle entity not found.
+    if (!entity) {
+      return Promise.resolve(null);
+    }
+
+    // Handle updating mongoose models
+    if (typeof entity.toObject === 'function') {
+      entity = entity.toObject();
+    } else if (typeof entity.toJSON === 'function') {
+      // Handle updating Sequelize models
+      entity = entity.toJSON();
+    }
+
+    debug(`${this.options.entity} found`);
+    return Promise.resolve(entity);
+  }
+
+  _updateEntity (entity, data) {
+    const options = this.options;
+    const name = options.name;
+    const id = entity[this.service.id];
+    debug(`Updating ${options.entity}: ${id}`);
+
+    const newData = {
+      [options.idField]: data.profile.id,
+      [name]: data
+    };
+
+    // Merge existing user data with new profile data
+    const updated = merge({}, entity, newData);
+    return this.service.update(id, updated, { oauth: { provider: name } });
+  }
+
+  _createEntity (data) {
+    const options = this.options;
+    const name = options.name;
+    const entity = {
+      [options.idField]: data.profile.id,
+      [name]: data
+    };
+
+    const id = entity[options.idField];
+    debug(`Creating new ${options.entity} with ${options.idField}: ${id}`);
+
+    return this.service.create(entity, { oauth: { provider: name } });
+  }
+
+  verify (req, accessToken, refreshToken, profile, done) {
+    debug('Checking credentials');
+    const options = this.options;
+    const query = {
+      [options.idField]: profile.id, // facebookId: profile.id
+      $limit: 1
+    };
+    const data = { profile, accessToken, refreshToken };
+    let existing;
+
+    // Check request object for an existing entity
+    if (req && req[options.entity]) {
+      existing = req[options.entity];
+    }
+
+    // Check the request that came from a hook for an existing entity
+    if (!existing && req && req.params && req.params[options.entity]) {
+      existing = req.params[options.entity];
+    }
+
+    // If there is already an entity on the request object (ie. they are
+    // already authenticated) attach the profile to the existing entity
+    // because they are likely "linking" social accounts/profiles.
+    if (existing) {
+      return this._updateEntity(existing, data)
+        .then(entity => done(null, entity))
+        .catch(error => error ? done(error) : done(null, error));
+    }
+
+    // Find or create the user since they could have signed up via facebook.
+    this.service
+      .find({ query })
+      .then(this._normalizeResult)
+      .then(entity => entity ? this._updateEntity(entity, data) : this._createEntity(data))
+      .then(entity => done(null, entity))
+      .catch(error => error ? done(error) : done(null, error));
+  }
+}
+
+export default OAuth1Vierifier;
--- packages/authentication-oauth1/test/index.test.js
+++ packages/authentication-oauth1/test/index.test.js
@@ -1,5 +1,13 @@
-import { expect } from 'chai';
-import plugin from '../src';
+import feathers from 'feathers';
+import memory from 'feathers-memory';
+import authentication from 'feathers-authentication';
+import oauth1, { Verifier } from '../src';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import Strategy from './fixtures/strategy';
+
+chai.use(sinonChai);
 
 describe('feathers-authentication-oauth1', () => {
   it('is CommonJS compatible', () => {
@@ -7,7 +15,229 @@ describe('feathers-authentication-oauth1', () => {
   });
 
   it('basic functionality', () => {
-    expect(typeof plugin).to.equal('function', 'It worked');
-    expect(plugin()).to.equal('feathers-authentication-oauth1');
+    expect(typeof oauth1).to.equal('function');
+  });
+
+  it('exposes the Verifier class', () => {
+    expect(typeof Verifier).to.equal('function');
+    expect(typeof oauth1.Verifier).to.equal('function');
+  });
+
+  describe('initialization', () => {
+    let app;
+    let config;
+    let globalConfig;
+
+    beforeEach(() => {
+      config = {
+        name: 'twitter',
+        Strategy,
+        consumerKey: '1234',
+        consumerSecret: 'secret'
+      };
+
+      globalConfig = {
+        secret: 'supersecret',
+        twitter: {
+          consumerKey: '1234',
+          consumerSecret: 'secret',
+          scope: ['user']
+        }
+      };
+
+      app = feathers();
+      app.set('host', 'localhost');
+      app.set('port', 3030);
+      app.use('/users', memory());
+      app.configure(authentication(globalConfig));
+    });
+
+    it('throws an error if passport has not been registered', () => {
+      expect(() => {
+        feathers().configure(oauth1());
+      }).to.throw();
+    });
+
+    it('throws an error if strategy name is missing', () => {
+      expect(() => {
+        delete config.name;
+        app.configure(oauth1(config));
+      }).to.throw();
+    });
+
+    it('throws an error if Strategy is missing', () => {
+      expect(() => {
+        delete config.Strategy;
+        app.configure(oauth1(config));
+      }).to.throw();
+    });
+
+    it('throws an error if consumerKey is missing', () => {
+      expect(() => {
+        delete config.consumerKey;
+        delete globalConfig.twitter.consumerKey;
+        feathers().configure(authentication(globalConfig)).configure(oauth1(config));
+      }).to.throw();
+    });
+
+    it('throws an error if consumerSecret is missing', () => {
+      expect(() => {
+        delete config.consumerSecret;
+        delete globalConfig.twitter.consumerSecret;
+        feathers().configure(authentication(globalConfig)).configure(oauth1(config));
+      }).to.throw();
+    });
+
+    it('registers the oauth1 passport strategy', () => {
+      sinon.spy(app.passport, 'use');
+      sinon.spy(config, 'Strategy');
+      app.configure(oauth1(config));
+      app.setup();
+
+      expect(config.Strategy).to.have.been.calledOnce;
+      expect(app.passport.use).to.have.been.calledWith(config.name);
+
+      app.passport.use.restore();
+      config.Strategy.restore();
+    });
+
+    describe('passport strategy options', () => {
+      let authOptions;
+      let args;
+
+      beforeEach(() => {
+        config.custom = true;
+        sinon.spy(config, 'Strategy');
+        app.configure(oauth1(config));
+        app.setup();
+        authOptions = app.get('auth');
+        args = config.Strategy.getCall(0).args[0];
+      });
+
+      afterEach(() => {
+        config.Strategy.restore();
+      });
+
+      it('sets path', () => {
+        expect(args.path).to.equal(`/auth/${config.name}`);
+      });
+
+      it('sets callbackURL', () => {
+        expect(args.callbackURL).to.equal(`http://localhost:3030/auth/${config.name}/callback`);
+      });
+
+      it('sets idField', () => {
+        expect(args.idField).to.equal(`${config.name}Id`);
+      });
+
+      it('sets entity', () => {
+        expect(args.entity).to.equal(authOptions.entity);
+      });
+
+      it('sets service', () => {
+        expect(args.service).to.equal(authOptions.service);
+      });
+
+      it('sets session', () => {
+        expect(args.session).to.equal(authOptions.session);
+      });
+
+      it('sets passReqToCallback', () => {
+        expect(args.passReqToCallback).to.equal(authOptions.passReqToCallback);
+      });
+
+      it('supports setting custom options', () => {
+        expect(args.custom).to.equal(true);
+      });
+    });
+
+    it('mixes in global config for strategy', () => {
+      delete config.consumerKey;
+      delete config.consumerSecret;
+      sinon.spy(config, 'Strategy');
+
+      app.configure(oauth1(config));
+      app.setup();
+
+      expect(config.Strategy.getCall(0).args[0].scope).to.deep.equal(['user']);
+      
+      config.Strategy.restore();
+    });
+
+    it('supports overriding default options', () => {
+      sinon.spy(config, 'Strategy');
+      config.entity = 'organization';
+      app.configure(oauth1(config));
+      app.setup();
+
+      expect(config.Strategy.getCall(0).args[0].entity).to.equal('organization');
+      
+      config.Strategy.restore();
+    });
+
+    it('registers express get route', () => {
+      sinon.spy(app, 'get');
+      app.configure(oauth1(config));
+      app.setup();
+
+      expect(app.get).to.have.been.calledWith(`/auth/${config.name}`);
+
+      app.get.restore();
+    });
+
+    it('registers express callback route', () => {
+      sinon.spy(app, 'get');
+      app.configure(oauth1(config));
+      app.setup();
+
+      expect(app.get).to.have.been.calledWith(`/auth/${config.name}/callback`);
+
+      app.get.restore();
+    });
+
+    describe('custom Verifier', () => {
+      it('throws an error if a verify function is missing', () => {
+        expect(() => {
+          class CustomVerifier {
+            constructor (app) {
+              this.app = app;
+            }
+          }
+          config.Verifier = CustomVerifier;
+          app.configure(oauth1(config));
+          app.setup();
+        }).to.throw();
+      });
+
+      it('verifies through custom verify function', () => {
+        const User = {
+          email: 'admin@feathersjs.com',
+          password: 'password'
+        };
+
+        const req = {
+          query: {},
+          body: Object.assign({}, User),
+          headers: {},
+          cookies: {}
+        };
+        class CustomVerifier extends Verifier {
+          verify (req, accessToken, refreshToken, profile, done) {
+            expect(accessToken).to.equal('mockAccessToken');
+            expect(refreshToken).to.equal('mockRefreshToken');
+            expect(profile).to.deep.equal({ name: 'Mocky Mockerson' });
+            done(null, User);
+          }
+        }
+
+        config.Verifier = CustomVerifier;
+        app.configure(oauth1(config));
+        app.setup();
+
+        return app.authenticate('twitter')(req).then(result => {
+          expect(result.data.user).to.deep.equal(User);
+        });
+      });
+    });
   });
 });
