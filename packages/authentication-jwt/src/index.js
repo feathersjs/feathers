8c12cf359d66e5492198b37d749955fbedb8f6aa
timelesshaze <timelesshaze@users.noreply.github.com>
2016-12-13 14:16:25 +0100

    add support for using the Bearer scheme in the Auth header

--- packages/authentication-jwt/src/index.js
+++ packages/authentication-jwt/src/index.js
@@ -48,6 +48,7 @@ export default function init (options = {}) {
     let strategyOptions = merge({
       secretOrKey: jwtSettings.secret,
       jwtFromRequest: ExtractJwt.fromExtractors([
+        ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
         ExtractJwt.fromHeader(jwtSettings.header.toLowerCase()),
         ExtractJwt.fromBodyField(jwtSettings.bodyKey)
       ])
