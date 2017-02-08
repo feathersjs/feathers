4dfd5dc482cc7182142abbe98dc2faa390ac0d9a
David Luecke <daff@neyeon.com>
2017-02-07 21:32:15 -0800

    Adding a meta.js with information about the available generators to pull into the CLI

---
+++ packages/generator-feathers/meta.js
@@ -0,0 +1,9 @@
+module.exports = {
+  app: 'Create a new Feathers application in the current folder',
+  authentication: 'Set up authentication for the current application',
+  connection: 'Initialize a new database connection',
+  hook: 'Create a new hook',
+  middleware: 'Create an Express middleware',
+  service: 'Generate a new service',
+  plugin: 'Create a new Feathers plugin'
+};
