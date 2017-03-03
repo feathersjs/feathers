23c2758c751dac5bb700413fb11d582d4059d1a0
David Luecke <daff@neyeon.com>
2017-03-02 19:55:47 -0800

    Add support for selecting application hooks and old event filter (#182)

--- packages/generator-feathers/generators/hook/index.js
+++ packages/generator-feathers/generators/hook/index.js
@@ -37,8 +37,14 @@ module.exports = class HookGenerator extends Generator {
     return ast.toSource();
   }
 
-  _addToService(serviceName, moduleName) {
-    const hooksFile = this.destinationPath(this.libDirectory, 'services', serviceName, `${serviceName}.hooks.js`);
+  _addToService(serviceName, hookName) {
+    let hooksFile = this.destinationPath(this.libDirectory, 'services', serviceName, `${serviceName}.hooks.js`);
+    let moduleName = `../../${hookName}`;
+
+    if(serviceName === '__app') {
+      hooksFile = this.destinationPath(this.libDirectory, 'app.hooks.js');
+      moduleName = `./${hookName}`;
+    }
 
     if(!this.fs.exists(hooksFile)) {
       throw new Error(`Can not add hook to the ${serviceName} hooks file ${hooksFile}. It does not exist.`);
@@ -79,7 +85,10 @@ module.exports = class HookGenerator extends Generator {
         name: 'services',
         message: 'What service(s) should this hook be for (select none to add it yourself)?\n',
         choices() {
-          return services.map(value => ({ value }));
+          return [{
+            name: 'Application wide (all services)',
+            value: '__app'
+          }].concat(services.map(value => ({ value })));
         },
         when(answers) {
           return answers.type !== null;
@@ -132,7 +141,7 @@ module.exports = class HookGenerator extends Generator {
 
     if(!this.fs.exists(mainFile) && context.type) {
       this.props.services.forEach(serviceName =>
-        this._addToService(serviceName, path.join('..', '..', 'hooks', context.kebabName))
+        this._addToService(serviceName, `hooks/${context.kebabName}`)
       );
     }
 
--- packages/generator-feathers/generators/hook/templates/hook.js
+++ packages/generator-feathers/generators/hook/templates/hook.js
@@ -1,11 +1,10 @@
 'use strict';
 
 // Use this hook to manipulate incoming or outgoing data.
-// For more information on hooks see: http://docs.feathersjs.com/hooks/readme.html
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 
 module.exports = function(options = {}) { // eslint-disable-line no-unused-vars
   return function(hook) {
-    hook.<%= camelName %> = true;
     console.log('\'<%= name %>\' hook ran');
 
     // Hooks can either return nothing or a promise
