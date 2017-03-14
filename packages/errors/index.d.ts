c98abf64922b602a12bc6194b10d23fb47a15de3
Daniel Constantin <daco@esmiley.dk>
2017-03-14 05:15:44 +0100

    Fix declarations for index.d.ts (#66)
    This commit should fix typescript compiler errors.
    
    Resolves: #63
    See also: #62

--- packages/errors/index.d.ts
+++ packages/errors/index.d.ts
@@ -1,6 +1,6 @@
 declare namespace FeathersErrors {
   class FeathersError extends Error {
-    constructor(msg: string | Error, name: string , code: number, className: String, data: any)
+    constructor(msg: string | Error, name: string, code: number, className: String, data: any)
   }
 
   class BadRequest extends FeathersError {
@@ -28,6 +28,10 @@ declare namespace FeathersErrors {
   }
 
   class NotAcceptable extends FeathersError {
+    constructor(msg: string | Error, data?: any);
+  }
+
+  class Timeout extends FeathersError {
     constructor(msg: string | Error, data?: any);
   }
 
@@ -64,10 +68,30 @@ declare namespace FeathersErrors {
   }
 }
 
-declare namespace FeathersErrors  {
-  types: FeathersErrors
-  function convert(error: any):FeathersError
-  errors: FeathersErrors
+declare namespace FeathersErrors {
+  interface ErrorContainer {
+    FeathersError
+    BadRequest
+    NotAuthenticated
+    PaymentError
+    Forbidden
+    NotFound
+    MethodNotAllowed
+    NotAcceptable
+    Timeout
+    Conflict
+    LengthRequired
+    Unprocessable
+    TooManyRequests
+    GeneralError
+    NotImplemented
+    BadGateway
+    Unavailable
+  }
+
+  function convert(error: any): FeathersError
+  const types: ErrorContainer
+  const errors: ErrorContainer
 }
 
-export = FeathersErrors
+export = FeathersErrors;
