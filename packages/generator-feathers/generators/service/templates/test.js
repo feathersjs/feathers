fd0d88c24b453ad6cedd09689a4454377c13df31
David Luecke <daff@neyeon.com>
2017-04-22 20:09:25 -0700

    Use proper path to get service in test (#210)

--- packages/generator-feathers/generators/service/templates/test.js
+++ packages/generator-feathers/generators/service/templates/test.js
@@ -3,7 +3,7 @@ const app = require('../../<%= libDirectory %>/app');
 
 describe('\'<%= name %>\' service', () => {
   it('registered the service', () => {
-    const service = app.service('<%= kebabName %>');
+    const service = app.service('<%= path %>');
 
     assert.ok(service, 'Registered the service');
   });
