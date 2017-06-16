453871660e2565b8d677760f05b78934dcced20c
David Luecke <daff@neyeon.com>
2017-06-15 23:20:53 -0700

    Improve order of generated middleware (#223)

--- packages/generator-feathers/generators/app/templates/src/middleware/index.js
+++ packages/generator-feathers/generators/app/templates/src/middleware/index.js
@@ -1,12 +1,6 @@
-const handler = require('feathers-errors/handler');
-const notFound = require('feathers-errors/not-found');
-
 module.exports = function () {
   // Add your custom middleware here. Remember, that
   // in Express the order matters, `notFound` and
   // the error handler have to go last.
-  const app = this;
-
-  app.use(notFound());
-  app.use(handler());
+  const app = this; // eslint-disable-line no-unused-vars
 };
