7b23de7c6f5da1142ad911d93db60196c77c684e
David Luecke <daff@neyeon.com>
2017-06-20 21:06:04 -0700

    Use Knex createTable workaround (#231)

--- packages/generator-feathers/generators/service/templates/model/knex.js
+++ packages/generator-feathers/generators/service/templates/model/knex.js
@@ -7,13 +7,18 @@
 module.exports = function (app) {
   const db = app.get('knexClient');
 
-  db.schema.createTableIfNotExists('<%= kebabName %>', table => {
-    table.increments('id');
-    table.string('text');
-  }).then(
-    () => console.log('Updated <%= kebabName %> table'),
-    e => console.error('Error updating <%= kebabName %> table', e)
-  );
+  db.schema.hasTable('<%= kebabName %>').then(exists => {
+    if(!exists) {
+      db.schema.createTable('<%= kebabName %>', table => {
+        table.increments('id');
+        table.string('text');
+      }).then(
+        () => console.log('Updated <%= kebabName %> table'),
+        e => console.error('Error updating <%= kebabName %> table', e)
+      );
+    }
+  });
+  
 
   return db;
 };
