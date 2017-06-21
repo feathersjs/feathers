4bc9fc02ab94172347bb6f8a294f8caaa653b0b5
Jeremy Fowler <jeremy.f76@gmail.com>
2017-06-21 12:08:55 -0500

    fixes several issues with update-entity w/ test cases (#531)

--- packages/authentication/test/socket/update-entity.test.js
+++ packages/authentication/test/socket/update-entity.test.js
@@ -1,5 +1,8 @@
 import { expect } from 'chai';
+import { ObjectID } from 'mongodb';
 import updateEntity from '../../src/socket/update-entity';
+
+const TEST_OBJECT_ID = '59499c9a901604391cab65f5';
 
 describe('Socket "Update Entity" Handler', function () {
   it('updates the passed-in entity for socket.io', function () {
@@ -103,5 +106,119 @@ describe('Socket "Update Entity" Handler', function () {
     updateEntity(user, { app });
 
     expect(app.primus.connections['my-socket'].request.feathers.user.email).to.equal('test@feathersjs.com');
+  });
+
+  it('gracefully handles unauthenticated connections', function () {
+    const app = {
+      get () {
+        return {
+          entity: 'user',
+          service: 'users'
+        };
+      },
+      io: {
+        sockets: {
+          sockets: {
+            'unauthenticated': {
+              request: {},
+              feathers: {}
+            },
+            'my-socket': {
+              feathers: {
+                user: { _id: 5, email: 'admin@feathersjs.com' }
+              }
+            }
+          }
+        }
+      },
+      services: {
+        users: {
+          id: '_id'
+        }
+      },
+      service (location) {
+        return this.services[location];
+      }
+    };
+    const user = { _id: 5, email: 'test@feathersjs.com' };
+
+    updateEntity(user, { app });
+
+    expect(app.io.sockets.sockets['my-socket'].feathers.user.email).to.equal('test@feathersjs.com');
+  });
+
+  it('updates the passed-in entity when the idField is an ObjectID', function () {
+    const app = {
+      get () {
+        return {
+          entity: 'user',
+          service: 'users'
+        };
+      },
+      io: {
+        sockets: {
+          sockets: {
+            'my-socket': {
+              feathers: {
+                user: { _id: new ObjectID(TEST_OBJECT_ID), email: 'admin@feathersjs.com' }
+              }
+            }
+          }
+        }
+      },
+      services: {
+        users: {
+          id: '_id'
+        }
+      },
+      service (location) {
+        return this.services[location];
+      }
+    };
+    const user = { _id: new ObjectID(TEST_OBJECT_ID), email: 'test@feathersjs.com' };
+
+    updateEntity(user, { app });
+
+    expect(app.io.sockets.sockets['my-socket'].feathers.user.email).to.equal('test@feathersjs.com');
+  });
+
+  it('socket entity should "deep equal" passed-in entity', function () {
+    const app = {
+      get () {
+        return {
+          entity: 'user',
+          service: 'users'
+        };
+      },
+      io: {
+        sockets: {
+          sockets: {
+            'my-socket': {
+              feathers: {
+                user: {
+                  _id: 5,
+                  email: 'admin@feathersjs.com',
+                  nested: { value: 1 },
+                  optional: true
+                }
+              }
+            }
+          }
+        }
+      },
+      services: {
+        users: {
+          id: '_id'
+        }
+      },
+      service (location) {
+        return this.services[location];
+      }
+    };
+    const user = { _id: 5, email: 'test@feathersjs.com', nested: { value: 3 } };
+
+    updateEntity(user, { app });
+
+    expect(app.io.sockets.sockets['my-socket'].feathers.user).to.deep.equal(user);
   });
 });
