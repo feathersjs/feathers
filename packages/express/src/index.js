5338cd8c0e70cc2c11a65f4a61e6aa23410c27fd
David Luecke <daff@neyeon.com>
2017-07-19 08:55:24 -0700

    Initial functionality

--- packages/express/src/index.js
+++
@@ -1,9 +0,0 @@
-// import errors from 'feathers-errors';
-import makeDebug from 'debug';
-
-const debug = makeDebug('feathers-express');
-
-export default function init () {
-  debug('Initializing feathers-express plugin');
-  return 'feathers-express';
-}
