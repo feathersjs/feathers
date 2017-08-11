b96d3e004812c996a5da853022c6e107c9486c74
micaksica <30743015+micaksica2@users.noreply.github.com>
2017-08-11 09:28:51 -0700

    Increase bcrypt cost factor, add future cost factor auto-optimization (#30)
    * Increase bcrypt default cost function; add auto-increase
    
    - Increases default bcrypt cost factor to 12
    - Increases bcrypt cost factor +1 automatically every 1.5 years from 1/1/17
      to maintain cost increases without needing updates
    
    * Force 0 multiplier in case of negative floor due to pre-1/1/17 clock skew
    
    * Move bcrypt constants to header per @daffl

--- packages/authentication-local/src/utils/hash.js
+++ packages/authentication-local/src/utils/hash.js
@@ -1,8 +1,16 @@
 import bcrypt from 'bcryptjs';
 
+const BCRYPT_WORK_FACTOR_BASE = 12;
+const BCRYPT_DATE_BASE = 1483228800000;
+const BCRYPT_WORK_INCREASE_INTERVAL = 47300000000;
+
 export default function hasher (password) {
   return new Promise((resolve, reject) => {
-    bcrypt.genSalt(10, function (error, salt) {
+    let BCRYPT_CURRENT_DATE = new Date().getTime();
+    let BCRYPT_WORK_INCREASE = Math.max(0, Math.floor((BCRYPT_CURRENT_DATE - BCRYPT_DATE_BASE) / BCRYPT_WORK_INCREASE_INTERVAL));
+    let BCRYPT_WORK_FACTOR = Math.min(19, BCRYPT_WORK_FACTOR_BASE + BCRYPT_WORK_INCREASE);
+
+    bcrypt.genSalt(BCRYPT_WORK_FACTOR, function (error, salt) {
       if (error) {
         return reject(error);
       }
