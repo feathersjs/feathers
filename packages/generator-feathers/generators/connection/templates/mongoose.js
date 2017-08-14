d8896f693b86eb06450d00c0022d9d8f2b7b4bdc
David Luecke <daff@neyeon.com>
2017-08-14 15:46:57 -0700

    Update connection code for latest MongoDB (#255)
    * Update connection code for latest MongoDB
    
    * Move options to the right spot

--- packages/generator-feathers/generators/connection/templates/mongoose.js
+++ packages/generator-feathers/generators/connection/templates/mongoose.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 module.exports = function () {
   const app = this;
 
-  mongoose.connect(app.get('mongodb'));
+  mongoose.connect(app.get('mongodb'), {
+    useMongoClient: true
+  });
   mongoose.Promise = global.Promise;
 
   app.set('mongooseClient', mongoose);
