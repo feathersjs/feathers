9c669c5ee55de8b8b0dd5b87afc0112165a36d6c
maximelkin <maxelkin@list.ru>
2017-09-01 06:04:59 +0300

    change template of default models for sequelize (access to types from DataTypes) (#267)

--- packages/generator-feathers/generators/service/templates/model/sequelize.js
+++ packages/generator-feathers/generators/service/templates/model/sequelize.js
@@ -1,12 +1,13 @@
 // See http://docs.sequelizejs.com/en/latest/docs/models-definition/
 // for more of what you can do here.
 const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
 
 module.exports = function (app) {
   const sequelizeClient = app.get('sequelizeClient');
   const <%= camelName %> = sequelizeClient.define('<%= kebabName %>', {
     text: {
-      type: Sequelize.STRING,
+      type: DataTypes.STRING,
       allowNull: false
     }
   }, {
