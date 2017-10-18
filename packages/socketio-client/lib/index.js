784f736be2e9f85471acc21e6f11f0433ac31f62
David Luecke <daff@neyeon.com>
2017-10-18 13:16:41 -0700

    Initial v3 SocketIO client

---
+++ packages/socketio-client/lib/index.js
@@ -0,0 +1,35 @@
+const Service = require('feathers-socket-commons/client');
+
+module.exports = function (connection, options) {
+  if (!connection) {
+    throw new Error('Socket.io connection needs to be provided');
+  }
+
+  const defaultService = function (name) {
+    const events = Object.keys(this.eventMappings || {})
+      .map(method => this.eventMappings[method]);
+
+    const settings = Object.assign({}, options, {
+      events,
+      name,
+      connection,
+      method: 'emit'
+    });
+
+    return new Service(settings);
+  };
+
+  const initialize = function () {
+    if (typeof this.defaultService === 'function') {
+      throw new Error('Only one default client provider can be configured');
+    }
+
+    this.io = connection;
+    this.defaultService = defaultService;
+  };
+
+  initialize.Service = Service;
+  initialize.service = defaultService;
+
+  return initialize;
+};
