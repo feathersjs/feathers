394e4ecbb50dea3f548e25d68d218eea5d5e6e83
David Luecke <daff@neyeon.com>
2017-10-19 12:36:29 -0700

    Initial commit of working Primus client

---
+++ packages/primus-client/README.md
@@ -0,0 +1,25 @@
+# @feathersjs/primus-client
+
+[![Build Status](https://travis-ci.org/feathersjs/primus-client.png?branch=master)](https://travis-ci.org/feathersjs/primus-client)
+[![Code Climate](https://codeclimate.com/github/feathersjs/primus-client/badges/gpa.svg)](https://codeclimate.com/github/feathersjs/primus-client)
+[![Test Coverage](https://codeclimate.com/github/feathersjs/primus-client/badges/coverage.svg)](https://codeclimate.com/github/feathersjs/primus-client/coverage)
+[![Dependency Status](https://img.shields.io/david/feathersjs/primus-client.svg?style=flat-square)](https://david-dm.org/feathersjs/primus-client)
+[![Download Status](https://img.shields.io/npm/dm/@feathersjs/primus-client.svg?style=flat-square)](https://www.npmjs.com/package/@feathersjs/primus-client)
+
+> Client services for Primus and feathers-primus
+
+## Installation
+
+```
+npm install @feathersjs/primus-client --save
+```
+
+## Documentation
+
+Please refer to the [@feathersjs/primus-client documentation](https://docs.feathersjs.com/api/primus.html#client) for more details.
+
+## License
+
+Copyright (c) 2017
+
+Licensed under the [MIT license](LICENSE).
---
+++ packages/primus-client/test/index.test.js
@@ -0,0 +1,75 @@
+const assert = require('assert');
+const feathers = require('feathers');
+const baseTests = require('feathers-commons/lib/test/client');
+
+const server = require('./server');
+const primus = require('../lib');
+
+describe('feathers-primus/client', () => {
+  let srv, socket;
+
+  const app = feathers().configure(primus({}, { timeout: 500 }));
+  const service = app.service('todos');
+
+  before(done => {
+    srv = server(primus => {
+      service.connection = socket = new primus.Socket('http://localhost:12012');
+    }).listen(12012);
+
+    srv.on('listening', () => done());
+  });
+
+  after(done => {
+    socket.socket.close();
+    srv.close(done);
+  });
+
+  it('throws an error with no connection', function () {
+    try {
+      feathers().configure(primus());
+      assert.ok(false);
+    } catch (e) {
+      assert.equal(e.message, 'Primus connection needs to be provided');
+    }
+  });
+
+  it('app has the primus attribute', function () {
+    assert.ok(app.primus);
+  });
+
+  it('throws an error when configured twice', function () {
+    try {
+      app.configure(primus({}));
+      assert.ok(false, 'Should never get here');
+    } catch (e) {
+      assert.equal(e.message, 'Only one default client provider can be configured');
+    }
+  });
+
+  it('can initialize a client instance', () => {
+    const init = primus(service.connection);
+    const todos = init.service('todos');
+
+    assert.ok(todos instanceof init.Service, 'Returned service is a client');
+
+    return todos.find().then(todos => assert.deepEqual(todos, [
+      {
+        text: 'some todo',
+        complete: false,
+        id: 0
+      }
+    ]));
+  });
+
+  it('times out with error when using non-existent service', () => {
+    const notMe = app.service('not-me');
+    // Hack because we didn't set the connection at the beginning
+    notMe.connection = socket;
+
+    return notMe.remove(1).catch(e => {
+      assert.equal(e.message, `Service 'not-me' not found`);
+    });
+  });
+
+  baseTests(service);
+});
