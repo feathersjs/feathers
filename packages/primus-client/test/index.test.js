8c6dda243d485ae7135a790cf3a4db5701889fa7
David Luecke <daff@neyeon.com>
2017-10-22 21:37:14 -0700

    Use scoped npm packages (#2)

--- packages/primus-client/test/index.test.js
+++ packages/primus-client/test/index.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const feathers = require('feathers');
+const feathers = require('@feathersjs/feathers');
 const baseTests = require('feathers-commons/lib/test/client');
 
 const server = require('./server');
