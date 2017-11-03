4d9b5caaa6f76cc2b01c5a0ba7220482d39cefed
David Luecke <daff@neyeon.com>
2017-11-03 16:47:28 -0700

    Move all channel handling into channels.js (#298)
    * Move all channel handling into channels.js
    
    * Use proper variable name
    
    * Remove yarn.lock
    
    * Fix a typo

--- packages/generator-feathers/generators/service/templates/types/mongodb.js
+++ packages/generator-feathers/generators/service/templates/types/mongodb.js
@@ -18,12 +18,4 @@ module.exports = function (app) {
   });
 
   service.hooks(hooks);
-
-  app.publish(() => {
-    // Here you can add event publishers to channels set up in `channels.js`
-    // To publish only for a specific event use `app.publish(eventname, () => {})`
-
-    // e.g. to publish all service events to all authenticated users use
-    // return app.channel('authenticated');
-  });
 };
--- packages/generator-feathers/generators/service/templates/types/rethinkdb.js
+++ packages/generator-feathers/generators/service/templates/types/rethinkdb.js
@@ -19,12 +19,4 @@ module.exports = function (app) {
   const service = app.service('<%= path %>');
 
   service.hooks(hooks);
-
-  app.publish(() => {
-    // Here you can add event publishers to channels set up in `channels.js`
-    // To publish only for a specific event use `app.publish(eventname, () => {})`
-
-    // e.g. to publish all service events to all authenticated users use
-    // return app.channel('authenticated');
-  });
 };
