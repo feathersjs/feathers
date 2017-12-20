642d48df883e61952e5ad68c43641355843a8171
David Luecke <daff@neyeon.com>
2017-12-20 10:09:29 -0800

    Publish to authenticated users by default (#324)

--- packages/generator-feathers/generators/app/templates/src/channels.js
+++ packages/generator-feathers/generators/app/templates/src/channels.js
@@ -40,8 +40,10 @@ module.exports = function(app) {
     // Here you can add event publishers to channels set up in `channels.js`
     // To publish only for a specific event use `app.publish(eventname, () => {})`
 
+    console.log('Publishing all events to all authenticated users. See `channels.js` and https://docs.feathersjs.com/api/channels.html for more information.'); // eslint-disable-line
+
     // e.g. to publish all service events to all authenticated users use
-    // return app.channel('authenticated');
+    return app.channel('authenticated');
   });
 
   // Here you can also add service specific event publishers
