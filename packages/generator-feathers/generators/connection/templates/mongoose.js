70e71c54323d61b0bb497bd5de567a84876722ee
Alexander Smirnoff <a.dobeerman@gmail.com>
2018-01-27 06:04:40 +1000

    Update mongoose.js (#330)
    The `useMongoClient` option is no longer necessary in mongoose 5.x, please remove it.

--- packages/generator-feathers/generators/connection/templates/mongoose.js
+++ packages/generator-feathers/generators/connection/templates/mongoose.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = function (app) {
-  mongoose.connect(app.get('mongodb'), {
-    useMongoClient: true
-  });
+  mongoose.connect(app.get('mongodb'), {});
   mongoose.Promise = global.Promise;
 
   app.set('mongooseClient', mongoose);
