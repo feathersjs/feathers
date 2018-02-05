53ab0190c412e44d2fab0834e089f52ff4bf564c
David Luecke <daff@neyeon.com>
2018-02-05 13:39:37 -0800

    Move to use transport-commons (#10)

--- packages/socketio-client/lib/index.js
+++ packages/socketio-client/lib/index.js
@@ -1,4 +1,4 @@
-const Service = require('@feathersjs/socket-commons/client');
+const Service = require('@feathersjs/transport-commons/client');
 
 function socketioClient (connection, options) {
   if (!connection) {
