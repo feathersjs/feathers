efdc08149b0ed2535d32df8550aed561b8ae62b2
Christian Laforte <claforte@users.noreply.github.com>
2018-02-13 11:43:21 -0500

    fix typo (#338)

--- packages/generator-feathers/generators/app/templates/src/hooks/logger.js
+++ packages/generator-feathers/generators/app/templates/src/hooks/logger.js
@@ -3,13 +3,13 @@
 // about the logger.
 const logger = require('winston');
 
-// To see more detailed messages, uncomment the following line
+// To see more detailed messages, uncomment the following line:
 // logger.level = 'debug';
 
 module.exports = function () {
   return context => {
     // This debugs the service call and a stringified version of the hook context
-    // You can customize the mssage (and logger) to your needs
+    // You can customize the message (and logger) to your needs
     logger.debug(`${context.type} app.service('${context.path}').${context.method}()`);
     
     if(typeof context.toJSON === 'function') {
