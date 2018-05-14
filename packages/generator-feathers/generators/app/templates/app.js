38d8457657f4626b1e658b14b3736d5d838f83e7
Aidan Dang <dang@aidan.gg>
2018-05-15 01:54:08 +1000

    Use cors after helmet. (#351)

--- packages/generator-feathers/generators/app/templates/app.js
+++ packages/generator-feathers/generators/app/templates/app.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const favicon = require('serve-favicon');
 const compress = require('compression');
-const cors = require('cors');
 const helmet = require('helmet');
+const cors = require('cors');
 const logger = require('winston');
 
 const feathers = require('@feathersjs/feathers');
@@ -20,9 +20,9 @@ const app = express(feathers());
 
 // Load app configuration
 app.configure(configuration());
-// Enable CORS, security, compression, favicon and body parsing
-app.use(cors());
+// Enable security, CORS, compression, favicon and body parsing
 app.use(helmet());
+app.use(cors());
 app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
