2858b9633ac3e740f36205b59e06d99a82427205
Joseph Paul <mail@jsph.pl>
2018-05-17 17:21:57 +0200

    Remove "and filters" from a comment in the service.js template (#354)
    Filters no longer exist (or at least, are not documented) in recent versions

--- packages/generator-feathers/generators/service/templates/service.js
+++ packages/generator-feathers/generators/service/templates/service.js
@@ -15,7 +15,7 @@ module.exports = function (app) {
   // Initialize our service with any options it requires
   app.use('/<%= path %>', createService(options));
 
-  // Get our initialized service so that we can register hooks and filters
+  // Get our initialized service so that we can register hooks
   const service = app.service('<%= path %>');
 
   service.hooks(hooks);
