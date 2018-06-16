27a60e3fa66cd7d4345c133c8f8d54f8a6d449a2
David Luecke <daff@neyeon.com>
2018-06-15 17:28:54 -0700

    Only log error stacks and add Auth0 scope (#367)

--- packages/generator-feathers/generators/authentication/index.js
+++ packages/generator-feathers/generators/authentication/index.js
@@ -107,6 +107,7 @@ module.exports = class AuthGenerator extends Generator {
 
         if(strategy === 'auth0') {
           strategyConfig.domain = 'mydomain.auth0.com';
+          strategyConfig.sopes = [ 'profile' ];
         }
 
         if (strategy === 'facebook') {
