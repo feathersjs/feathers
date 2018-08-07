de2eba8d9b4c8b86f0494aa86be21216fcf09c7d
David Luecke <daff@neyeon.com>
2018-08-07 08:16:55 -0700

    Fix typo

--- packages/generator-feathers/generators/app/templates/src/logger.js
+++ packages/generator-feathers/generators/app/templates/src/logger.js
@@ -1,6 +1,6 @@
 const { createLogger, format, transports } = require('winston');
 
-// Configure the Winston logger. For the complete documentation seee https://github.com/winstonjs/winston
+// Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
 const logger = createLogger({
   // To see more detailed errors, change this to 'debug'
   level: 'info',
