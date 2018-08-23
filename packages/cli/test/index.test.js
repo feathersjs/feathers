9bea5bad40431e9321e4200a3e8f58d17e7738e7
Kévin Berthommier <kevin.bertho@gmail.com>
2018-08-23 22:45:56 +0200

    cli: shorten the test

--- packages/cli/test/index.test.js
+++ packages/cli/test/index.test.js
@@ -18,22 +18,10 @@ describe('@feathersjs/cli', () => {
     exec(`node ${path.join(__dirname, '../bin/feathers.js')} generate`, (err, stdout, stderr) => {
       assert.equal(err, null);
       assert.equal(stderr, '');
-      assert.equal(stdout, '\n  Usage: feathers generate [type]\n\n\n' +
-        '  Options:\n\n' +
-        '    -V, --version  output the version number\n' +
-        '    -h, --help     output usage information\n\n\n' +
-        '  Commands:\n\n' +
-        '    generate|g [type]  Run a generator. Type can be\n' +
-        '    \t• app - Create a new Feathers application in the current folder\n' +
-        '    \t• authentication - Set up authentication for the current application\n' +
-        '    \t• connection - Initialize a new database connection\n' +
-        '    \t• hook - Create a new hook\n' +
-        '    \t• middleware - Create an Express middleware\n' +
-        '    \t• secret - Generate a new authentication secret\n' +
-        '    \t• service - Generate a new service\n' +
-        '    \t• plugin - Create a new Feathers plugin\n' +
-        '    \n    upgrade|u          Try to automatically upgrade to the latest Feathers version\n' +
-        '    *                \n');
+      assert.ok(
+        /Usage: feathers generate \[type\]/.test(stdout),
+        'stdout should contain "Usage: feathers generate [type]"'
+      );
 
       done();
     });
