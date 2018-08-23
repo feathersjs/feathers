f8b4159a5fbdf8574dd2baf57d7ae89798b1bcd2
Kévin Berthommier <kevin.bertho@gmail.com>
2018-08-23 22:17:26 +0200

    update cli

--- packages/cli/test/index.test.js
+++ packages/cli/test/index.test.js
@@ -1,4 +1,6 @@
 const assert = require('assert');
+const { exec } = require('child_process');
+const path = require('path');
 const cli = require('../lib');
 
 describe('@feathersjs/cli', () => {
@@ -6,12 +8,31 @@ describe('@feathersjs/cli', () => {
     assert.equal(typeof require('../lib'), 'function');
   });
 
-  it('basic functionality', done => {
+  it('basic functionality', () => {
     assert.equal(typeof cli, 'function', 'It worked');
-    done();
   });
 
-  it('runs the program with `generate` argument', () => {
-    cli([ null, __filename, 'generate' ]);
+  it('runs the program with `generate` argument', done => {
+    exec(`node ${path.join(__dirname, '../bin/feathers.js')} generate`, (err, stdout) => {
+      assert.equal(err, null);
+      assert.equal(stdout, '\n  Usage: feathers generate [type]\n\n\n' +
+        '  Options:\n\n' +
+        '    -V, --version  output the version number\n' +
+        '    -h, --help     output usage information\n\n\n' +
+        '  Commands:\n\n' +
+        '    generate|g [type]  Run a generator. Type can be\n' +
+        '    \t• app - Create a new Feathers application in the current folder\n' +
+        '    \t• authentication - Set up authentication for the current application\n' +
+        '    \t• connection - Initialize a new database connection\n' +
+        '    \t• hook - Create a new hook\n' +
+        '    \t• middleware - Create an Express middleware\n' +
+        '    \t• secret - Generate a new authentication secret\n' +
+        '    \t• service - Generate a new service\n' +
+        '    \t• plugin - Create a new Feathers plugin\n' +
+        '    \n    upgrade|u          Try to automatically upgrade to the latest Feathers version\n' +
+        '    *                \n');
+
+      done();
+    });
   });
 });
