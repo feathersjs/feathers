9c14e0f73473bee09fdfe81c441e7626ac3eaef7
testless <39350336+testless@users.noreply.github.com>
2018-09-20 11:33:28 +0200

    Consider that hook.params might be falsy

--- packages/authentication-client/lib/hooks/populate-access-token.js
+++ packages/authentication-client/lib/hooks/populate-access-token.js
@@ -1,10 +1,12 @@
 module.exports = function populateAccessToken () {
   return function (hook) {
     const app = hook.app;
-
+    
     if (hook.type !== 'before') {
       return Promise.reject(new Error(`The 'populateAccessToken' hook should only be used as a 'before' hook.`));
     }
+    
+    hook.params = hook.params || {}
 
     Object.assign(hook.params, { accessToken: app.get('accessToken') });
 
