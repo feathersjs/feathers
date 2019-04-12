42b9f242808e0e665423fe15641f1cee24b37ad8
Jordan Tucker <jordanbtucker@gmail.com>
2019-04-11 23:58:13 -0500

    fix: Use `export =` in rest-client typings

--- packages/rest-client/index.d.ts
+++ packages/rest-client/index.d.ts
@@ -1,32 +1,40 @@
 // todo: get rid of all the anys
 
-export default function feathersRestClient (base?: string): Transport;
+declare const restClient: FeathersRestClient;
+export = restClient;
 
-export interface HandlerResult extends Function {
-    /**
-     * initialize service
-     */
-    (): void;
-
-    /**
-     * Transport Service
-     */
-    Service: any;
-
-    /**
-     * default Service
-     */
-    service: any;
+interface FeathersRestClient {
+    (base?: string): restClient.Transport;
+    default: FeathersRestClient;
 }
 
-export type Handler = (connection: any, options?: any) => () => HandlerResult;
+declare namespace restClient {
+    interface HandlerResult extends Function {
+        /**
+         * initialize service
+         */
+        (): void;
 
-export interface Transport {
-    jquery: Handler;
-    superagent: Handler;
-    request: Handler;
-    fetch: Handler;
-    axios: Handler;
-    angular: Handler;
-    angularHttpClient: Handler;
+        /**
+         * Transport Service
+         */
+        Service: any;
+
+        /**
+         * default Service
+         */
+        service: any;
+    }
+
+    type Handler = (connection: any, options?: any) => () => HandlerResult;
+
+    interface Transport {
+        jquery: Handler;
+        superagent: Handler;
+        request: Handler;
+        fetch: Handler;
+        axios: Handler;
+        angular: Handler;
+        angularHttpClient: Handler;
+    }
 }
