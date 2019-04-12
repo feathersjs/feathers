2eeb3e12b49fb5e059d869b6d383d9f18f44582a
Jordan Tucker <jordanbtucker@gmail.com>
2019-04-12 01:08:41 -0500

    fix: Add SOCKET_KEY to socket.io typings

--- packages/socketio/index.d.ts
+++ packages/socketio/index.d.ts
@@ -4,15 +4,16 @@ import io from 'socket.io';
 declare const socketio: FeathersSocketIO;
 export = socketio;
 
-declare module '@feathersjs/feathers' {
-  interface Application<ServiceTypes = any> {
-    listen (port: number): http.Server;
-  }
-}
-
 interface FeathersSocketIO {
   (callback?: (io: io.Server) => void): () => void;
   (options: number | io.ServerOptions, callback?: (io: io.Server) => void): () => void;
   (port: number, options?: io.ServerOptions, callback?: (io: io.Server) => void): () => void;
+  readonly SOCKET_KEY: unique symbol;
   default: FeathersSocketIO;
+}
+
+declare module '@feathersjs/feathers' {
+  interface Application<ServiceTypes = any> {
+    listen (port: number): http.Server;
+  }
 }
