9b9b43ff09af1080e4aaa537064bac37b881c9a2
David Luecke <daff@neyeon.com>
2019-10-25 19:09:25 -0700

    fix: Only initialize default Express session if oAuth is actually used (#1648)

--- packages/authentication-oauth/src/utils.ts
+++ packages/authentication-oauth/src/utils.ts
@@ -1,21 +1,15 @@
 import { RequestHandler } from 'express';
-import session from 'express-session';
 import { Application } from '@feathersjs/feathers';
 
 export interface OauthSetupSettings {
   authService?: string;
+  expressSession?: RequestHandler;
   linkStrategy: string;
-  expressSession: RequestHandler;
 }
 
 export const getDefaultSettings = (_app: Application, other?: Partial<OauthSetupSettings>) => {
   const defaults: OauthSetupSettings = {
     linkStrategy: 'jwt',
-    expressSession: session({
-      secret: Math.random().toString(36).substring(7),
-      saveUninitialized: true,
-      resave: true
-    }),
     ...other
   };
 
