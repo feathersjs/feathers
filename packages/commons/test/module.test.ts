1696794974caf46784c92511247b733700f5a42e
David Luecke <daff@neyeon.com>
2019-12-26 16:31:57 -0800

    chore: Update all dependencies and remove remaining Sinon and Chai usage (#1765)

--- packages/commons/test/module.test.ts
+++ packages/commons/test/module.test.ts
@@ -1,28 +1,28 @@
-import { expect } from 'chai';
+import { strict as assert } from 'assert';
 import { _ } from '../src';
 
 describe('module', () => {
   it('is commonjs compatible', () => {
     const commons = require('../lib');
 
-    expect(typeof commons).to.equal('object');
-    expect(typeof commons.stripSlashes).to.equal('function');
-    expect(typeof commons.hooks).to.equal('object');
-    expect(typeof commons._).to.equal('object');
+    assert.equal(typeof commons, 'object');
+    assert.equal(typeof commons.stripSlashes, 'function');
+    assert.equal(typeof commons.hooks, 'object');
+    assert.equal(typeof commons._, 'object');
   });
 
   it('exposes lodash methods under _', () => {
-    expect(typeof _.each).to.equal('function');
-    expect(typeof _.some).to.equal('function');
-    expect(typeof _.every).to.equal('function');
-    expect(typeof _.keys).to.equal('function');
-    expect(typeof _.values).to.equal('function');
-    expect(typeof _.isMatch).to.equal('function');
-    expect(typeof _.isEmpty).to.equal('function');
-    expect(typeof _.isObject).to.equal('function');
-    expect(typeof _.extend).to.equal('function');
-    expect(typeof _.omit).to.equal('function');
-    expect(typeof _.pick).to.equal('function');
-    expect(typeof _.merge).to.equal('function');
+    assert.equal(typeof _.each, 'function');
+    assert.equal(typeof _.some, 'function');
+    assert.equal(typeof _.every, 'function');
+    assert.equal(typeof _.keys, 'function');
+    assert.equal(typeof _.values, 'function');
+    assert.equal(typeof _.isMatch, 'function');
+    assert.equal(typeof _.isEmpty, 'function');
+    assert.equal(typeof _.isObject, 'function');
+    assert.equal(typeof _.extend, 'function');
+    assert.equal(typeof _.omit, 'function');
+    assert.equal(typeof _.pick, 'function');
+    assert.equal(typeof _.merge, 'function');
   });
 });
--- packages/commons/test/utils.test.ts
+++ packages/commons/test/utils.test.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-expression */
 
-import { expect } from 'chai';
+import { strict as assert } from 'assert';
 
 import {
   _,
@@ -12,154 +12,152 @@ import {
 
 describe('@feathersjs/commons utils', () => {
   it('stripSlashes', () => {
-    expect(stripSlashes('some/thing')).to.equal('some/thing');
-    expect(stripSlashes('/some/thing')).to.equal('some/thing');
-    expect(stripSlashes('some/thing/')).to.equal('some/thing');
-    expect(stripSlashes('/some/thing/')).to.equal('some/thing');
-    expect(stripSlashes('//some/thing/')).to.equal('some/thing');
-    expect(stripSlashes('//some//thing////')).to.equal('some//thing');
+    assert.equal(stripSlashes('some/thing'), 'some/thing');
+    assert.equal(stripSlashes('/some/thing'), 'some/thing');
+    assert.equal(stripSlashes('some/thing/'), 'some/thing');
+    assert.equal(stripSlashes('/some/thing/'), 'some/thing');
+    assert.equal(stripSlashes('//some/thing/'), 'some/thing');
+    assert.equal(stripSlashes('//some//thing////'), 'some//thing');
   });
 
   it('isPromise', () => {
-    expect(isPromise(Promise.resolve())).to.equal(true);
-    expect(isPromise({
+    assert.equal(isPromise(Promise.resolve()), true);
+    assert.ok(isPromise({
       then () {}
-    })).to.equal(true);
-    expect(isPromise(null)).to.equal(false);
+    }));
+    assert.equal(isPromise(null), false);
   });
 
   it('createSymbol', () => {
-    expect(typeof createSymbol('a test')).to.equal('symbol');
+    assert.equal(typeof createSymbol('a test'), 'symbol');
   });
 
   describe('_', () => {
     it('isObject', () => {
-      expect(_.isObject({})).to.equal(true);
-      expect(_.isObject([])).to.equal(false);
-      expect(_.isObject(null)).to.equal(false);
+      assert.equal(_.isObject({}), true);
+      assert.equal(_.isObject([]), false);
+      assert.equal(_.isObject(null), false);
     });
 
     it('isObjectOrArray', () => {
-      expect(_.isObjectOrArray({})).to.equal(true);
-      expect(_.isObjectOrArray([])).to.equal(true);
-      expect(_.isObjectOrArray(null)).to.equal(false);
+      assert.equal(_.isObjectOrArray({}), true);
+      assert.equal(_.isObjectOrArray([]), true);
+      assert.equal(_.isObjectOrArray(null), false);
     });
 
     it('each', () => {
       _.each({ hi: 'there' }, (value, key) => {
-        expect(key).to.equal('hi');
-        expect(value).to.equal('there');
+        assert.equal(key, 'hi');
+        assert.equal(value, 'there');
       });
 
       _.each([ 'hi' ], (value, key) => {
-        expect(key).to.equal(0);
-        expect(value).to.equal('hi');
+        assert.equal(key, 0);
+        assert.equal(value, 'hi');
       });
 
-      _.each('moo', () => expect(false)
-        .to.equal(true, 'Should never get here')
-      );
+      _.each('moo', () => assert.fail('Should never get here'));
     });
 
     it('some', () => {
-      expect(_.some([ 'a', 'b' ], current => current === 'a')).to.be.ok;
-      expect(!_.some([ 'a', 'b' ], current => current === 'c')).to.be.ok;
+      assert.ok(_.some([ 'a', 'b' ], current => current === 'a'));
+      assert.ok(!_.some([ 'a', 'b' ], current => current === 'c'));
     });
 
     it('every', () => {
-      expect(_.every([ 'a', 'a' ], current => current === 'a')).to.be.ok;
-      expect(!_.every([ 'a', 'b' ], current => current === 'a')).to.be.ok;
+      assert.ok(_.every([ 'a', 'a' ], current => current === 'a'));
+      assert.ok(!_.every([ 'a', 'b' ], current => current === 'a'));
     });
 
     it('keys', () => {
       const data = { hi: 'there', name: 'David' };
-      expect(_.keys(data)).to.deep.equal([ 'hi', 'name' ]);
+      assert.deepEqual(_.keys(data), [ 'hi', 'name' ]);
     });
 
     it('values', () => {
       const data = { hi: 'there', name: 'David' };
-      expect(_.values(data)).to.deep.equal([ 'there', 'David' ]);
+      assert.deepEqual(_.values(data), [ 'there', 'David' ]);
     });
 
     it('isMatch', () => {
-      expect(_.isMatch({
+      assert.ok(_.isMatch({
         test: 'me', hi: 'you', more: true
       }, {
         test: 'me', hi: 'you'
-      })).to.be.ok;
+      }));
 
-      expect(!_.isMatch({
+      assert.ok(!_.isMatch({
         test: 'me', hi: 'you', more: true
       }, {
         test: 'me', hi: 'there'
-      })).to.be.ok;
+      }));
     });
 
     it('isEmpty', () => {
-      expect(_.isEmpty({})).to.be.ok;
-      expect(!_.isEmpty({ name: 'David' })).to.be.ok;
+      assert.ok(_.isEmpty({}));
+      assert.ok(!_.isEmpty({ name: 'David' }));
     });
 
     it('extend', () => {
-      expect(_.extend({ hi: 'there' }, { name: 'david' })).to.deep.equal({
+      assert.deepEqual(_.extend({ hi: 'there' }, { name: 'david' }), {
         hi: 'there',
         name: 'david'
       });
     });
 
     it('omit', () => {
-      expect(_.omit({
+      assert.deepEqual(_.omit({
         name: 'David',
         first: 1,
         second: 2
-      }, 'first', 'second')).to.deep.equal({
+      }, 'first', 'second'), {
         name: 'David'
       });
     });
 
     it('pick', () => {
-      expect(_.pick({
+      assert.deepEqual(_.pick({
         name: 'David',
         first: 1,
         second: 2
-      }, 'first', 'second')).to.deep.equal({
+      }, 'first', 'second'), {
         first: 1,
         second: 2
       });
 
-      expect(_.pick({
+      assert.deepEqual(_.pick({
         name: 'David',
         first: 1
-      }, 'first', 'second')).to.deep.equal({
+      }, 'first', 'second'), {
         first: 1
       });
     });
 
     it('merge', () => {
-      expect(_.merge({ hi: 'there' }, { name: 'david' })).to.deep.equal({
+      assert.deepEqual(_.merge({ hi: 'there' }, { name: 'david' }), {
         hi: 'there',
         name: 'david'
       });
 
-      expect(_.merge({}, {
+      assert.deepEqual(_.merge({}, {
         name: 'david',
         nested: { obj: true }
-      })).to.deep.equal({
+      }), {
         name: 'david',
         nested: { obj: true }
       });
 
-      expect(_.merge({ name: 'david' }, {})).to.deep.equal({
+      assert.deepEqual(_.merge({ name: 'david' }, {}), {
         name: 'david'
       });
 
-      expect(_.merge({
+      assert.deepEqual(_.merge({
         hi: 'there',
         my: {
           name: { is: 'david' },
           number: { is: 1 }
         }
-      }, { my: { name: { is: 'eric' } } })).to.deep.equal({
+      }, { my: { name: { is: 'eric' } } }), {
         hi: 'there',
         my: {
           number: { is: 1 },
@@ -167,7 +165,7 @@ describe('@feathersjs/commons utils', () => {
         }
       });
 
-      expect(_.merge('hello', {})).to.equal('hello');
+      assert.equal(_.merge('hello', {}), 'hello');
     });
   });
 
@@ -190,44 +188,44 @@ describe('@feathersjs/commons utils', () => {
 
     it('when in development mode returns the correct url', () => {
       const uri = makeUrl('test', mockApp);
-      expect(uri).to.equal('http://feathersjs.com:3030/test');
+      assert.equal(uri, 'http://feathersjs.com:3030/test');
     });
 
     it('when in test mode returns the correct url', () => {
       mockApp.env = 'test';
       const uri = makeUrl('test', mockApp);
-      expect(uri).to.equal('http://feathersjs.com:3030/test');
+      assert.equal(uri, 'http://feathersjs.com:3030/test');
     });
 
     it('when in production mode returns the correct url', () => {
       mockApp.env = 'production';
       const uri = makeUrl('test', mockApp);
-      expect(uri).to.equal('https://feathersjs.com/test');
+      assert.equal(uri, 'https://feathersjs.com/test');
     });
 
     it('when path is not provided returns a default url', () => {
       const uri = makeUrl(null, mockApp);
-      expect(uri).to.equal('http://feathersjs.com:3030/');
+      assert.equal(uri, 'http://feathersjs.com:3030/');
     });
 
     it('when app is not defined returns the correct url', () => {
       const uri = makeUrl('test');
-      expect(uri).to.equal('http://localhost:3030/test');
+      assert.equal(uri, 'http://localhost:3030/test');
     });
 
     it('strips leading slashes on path', () => {
       const uri = makeUrl('/test');
-      expect(uri).to.equal('http://localhost:3030/test');
+      assert.equal(uri, 'http://localhost:3030/test');
     });
 
     it('strips trailing slashes on path', () => {
       const uri = makeUrl('test/');
-      expect(uri).to.equal('http://localhost:3030/test');
+      assert.equal(uri, 'http://localhost:3030/test');
     });
 
     it('works with query strings', () => {
       const uri = makeUrl('test?admin=true');
-      expect(uri).to.equal('http://localhost:3030/test?admin=true');
+      assert.equal(uri, 'http://localhost:3030/test?admin=true');
     });
   });
 });
