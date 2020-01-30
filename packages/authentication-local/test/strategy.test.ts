2ca0f91b419e26e71dcc02aace9e783bb3318468
Sean Nicholas Dieterle <nicholas.dieterle@ece24.net>
2020-01-30 10:32:14 +0000

    fix: typo in password

--- packages/authentication-local/test/strategy.test.ts
+++ packages/authentication-local/test/strategy.test.ts
@@ -151,7 +151,7 @@ describe('@feathersjs/authentication-local/strategy', () => {
 
     assert.ok(accessToken);
     assert.strictEqual(authResult.user.email, email);
-    assert.strictEqual(authResult.user.passsword, undefined);
+    assert.strictEqual(authResult.user.password, undefined);
     assert.ok(authResult.user.fromGet);
 
     const decoded = await authService.verifyAccessToken(accessToken);
