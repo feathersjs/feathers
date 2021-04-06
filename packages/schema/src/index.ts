f05c15ec3d89d98287a7754adfe958caf3065c27
David Luecke <daff@neyeon.com>
2021-04-06 09:58:15 -0700

    Some initial doodling

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -1,3 +1,47 @@
+import { FromSchema, JSONSchema } from 'json-schema-to-ts';
+
+const dogSchema = {
+  type: 'object',
+  properties: {
+    name: {
+      type: 'string',
+      resolve (value: string) {
+        return value;
+      } 
+    },
+    age: { type: 'integer' },
+    hobbies: { type: 'array', items: { type: 'string' } },
+    favoriteFood: { enum: ['pizza', 'taco', 'fries'] },
+  },
+  required: ['name', 'age'],
+} as const;
+
+type Dog = FromSchema<typeof dogSchema>;
+
+type Resolver<T, C, O = any> = (obj: O, context: C) => Promise<T>;
+type PropertyResolver<V, T, C, O = any> = (value: V, obj: O, context: C) => Promise<T>;
+
+export class Schema<S extends JSONSchema, C, T = FromSchema<S>> {
+  constructor (
+    public definition: S,
+    public resolvers: Resolver<T, C>[]
+  ) {}
+
+  async resolve (obj: any, context: C) {
+    let result = obj;
+
+    for (const resolve of this.resolvers) {
+      result = await resolve(result, context);
+    }
+
+    return result;
+  }
+}
+
 export function hello () {
   return 'Hello';
+}
+
+export function schema <S> (schema: S extends JSONSchema) {
+
 }
--- packages/schema/test/index.test.ts
+++ packages/schema/test/index.test.ts
@@ -4,5 +4,22 @@ import { hello } from '../src';
 describe('@feathersjs/schema', () => {
   it('initializes', async () => {
     assert.strictEqual(hello(), 'Hello');
+
+    const app = feathers();
+
+
+    app.use('/messages', memory(), {
+      schema: {
+        query: {},
+        data: {},
+        result: {},
+        methods: {
+          create: {
+            data: {},
+            result: {}
+          }
+        }
+      }
+    })
   });
 });
