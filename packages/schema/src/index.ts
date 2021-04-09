15dc6a9dba86caf07aab5fa273852988d165a3cd
David Luecke <daff@neyeon.com>
2021-04-08 22:05:17 -0700

    Fix prototype of @feathersjs/schema

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -1,47 +1,96 @@
+import Ajv, { AsyncValidateFunction } from 'ajv';
+import { JSONSchema6 } from 'json-schema';
+import { getServiceOptions, HookContext, NextFunction } from '@feathersjs/feathers';
 import { FromSchema, JSONSchema } from 'json-schema-to-ts';
 
-const dogSchema = {
-  type: 'object',
-  properties: {
-    name: {
-      type: 'string',
-      resolve (value: string) {
-        return value;
-      } 
-    },
-    age: { type: 'integer' },
-    hobbies: { type: 'array', items: { type: 'string' } },
-    favoriteFood: { enum: ['pizza', 'taco', 'fries'] },
-  },
-  required: ['name', 'age'],
-} as const;
+export type Infer<T extends { _type: any }> = T['_type'];
 
-type Dog = FromSchema<typeof dogSchema>;
+export type PropertyResolver = (value: any, obj: any, context: any) => any;
 
-type Resolver<T, C, O = any> = (obj: O, context: C) => Promise<T>;
-type PropertyResolver<V, T, C, O = any> = (value: V, obj: O, context: C) => Promise<T>;
+export type PropertyResolverMap = {
+  [key: string]: PropertyResolver;
+}
 
-export class Schema<S extends JSONSchema, C, T = FromSchema<S>> {
-  constructor (
-    public definition: S,
-    public resolvers: Resolver<T, C>[]
-  ) {}
+export class Schema<S extends JSONSchema> {
+  ajv: Ajv;
+  validate: AsyncValidateFunction;
+  definition: JSONSchema6;
+  resolvers: PropertyResolverMap;
+  readonly _type!: FromSchema<S>;
 
-  async resolve (obj: any, context: C) {
-    let result = obj;
+  constructor (definition: S, ajv: Ajv = new Ajv({
+    coerceTypes: true,
+    strict: false
+  })) {
+    this.ajv = ajv;
+    this.definition = definition as JSONSchema6;
+    this.validate = this.ajv.compile({
+      $async: true,
+      ...this.definition
+    });
 
-    for (const resolve of this.resolvers) {
-      result = await resolve(result, context);
-    }
+    const { properties } = this.definition;
 
-    return result;
+    this.resolvers = Object.keys(properties).reduce((res, name) => {
+      const { resolve } = properties[name] as any;
+
+      if (typeof resolve === 'function') {
+        res[name] = resolve;
+      }
+
+      return res;
+    }, {} as PropertyResolverMap);
+  }
+
+  async resolve (obj: any, context?: any) {
+    const data: any = await this.validate(obj);
+    const result = { ...data };
+
+    await Promise.all(Object.keys(this.resolvers).map(async name => {
+      const resolved = await this.resolvers[name](data[name], data, context);
+
+      if (resolved === undefined) {
+        delete result[name];
+      } else {
+        result[name] = resolved;
+      }
+    }));
+
+    return result as Infer<Schema<S>>;
   }
 }
 
-export function hello () {
-  return 'Hello';
+export function schema <S extends JSONSchema> (schema: S) {
+  return new Schema(schema);
 }
 
-export function schema <S> (schema: S extends JSONSchema) {
+export function resolveSchemas () {
+  return async (context: HookContext, next: NextFunction) => {
+    const { params: { query = {} }, data } = context;
+    const { schema = {} } = getServiceOptions(context.service);
 
+    if (schema.query) {
+      context.params.query = await schema.query.resolve(query, context);
+    }
+
+    if (schema.data && data !== undefined) {
+      context.data = await schema.data.resolve(data, context);
+    }
+
+    await next();
+
+    if (schema.result) {
+      context.result = await schema.result.resolve(context.result, context);
+    }
+  }
+}
+
+declare module '@feathersjs/feathers/lib/declarations' {
+  export interface ServiceOptions {
+    schema?: {
+      query?: Schema<any>,
+      data?: Schema<any>,
+      result?: Schema<any>
+    }
+  }
 }
--- packages/schema/test/index.test.ts
+++ packages/schema/test/index.test.ts
@@ -1,25 +1,112 @@
 import assert from 'assert';
-import { hello } from '../src';
+import { feathers, HookContext } from '@feathersjs/feathers';
+import { memory } from '@feathersjs/adapter-memory';
+
+import { schema, resolveSchemas, Infer } from '../src';
 
 describe('@feathersjs/schema', () => {
-  it('initializes', async () => {
-    assert.strictEqual(hello(), 'Hello');
-
-    const app = feathers();
-
-
-    app.use('/messages', memory(), {
-      schema: {
-        query: {},
-        data: {},
-        result: {},
-        methods: {
-          create: {
-            data: {},
-            result: {}
+  it('simple JSON schema and resolving', async () => {
+    const messageSchema = schema({
+      type: 'object',
+      required: [ 'text' ],
+      additionalProperties: false,
+      properties: {
+        text: {
+          type: 'string'
+        },
+        read: {
+          type: 'boolean'
+        },
+        upvotes: {
+          type: 'number'
+        },
+        user: {
+          type: 'object',
+          resolve () {
+            return { name: 'Dave' };
           }
         }
       }
-    })
+    } as const);
+    type Message = Infer<typeof messageSchema>;
+
+    const message: Message = await messageSchema.resolve({
+      text: 'hi',
+      read: 0,
+      upvotes: '10'
+    });
+
+    assert.deepStrictEqual(message, {
+      text: 'hi',
+      read: false,
+      upvotes: 10,
+      user: { name: 'Dave' }
+    });
+  });
+
+  it ('schemas in Feathers app', async () => {
+    const messageSchema = schema({
+      type: 'object',
+      required: [ 'text', 'userId' ],
+      additionalProperties: false,
+      properties: {
+        id: { type: 'number' },
+        text: { type: 'string' },
+        userId: { type: 'number' },
+        user: {
+          type: 'object',
+          resolve (_value: any, message: any, context: HookContext) {
+            if (context.result === undefined) {
+              return undefined;
+            }
+
+            return context.app.service('users').get(message.userId);
+          }
+        }
+      }
+    } as const);
+    const userSchema = schema({
+      type: 'object',
+      required: [ 'email' ],
+      additionalProperties: false,
+      properties: {
+        id: { type: 'number' },
+        email: { type: 'string' }
+      }
+    } as const);
+
+    const app = feathers()
+      .use('/messages', memory(), {
+        schema: {
+          result: messageSchema,
+          data: messageSchema
+        }
+      })
+      .use('/users', memory(), {
+        schema: {
+          result: userSchema,
+          data: userSchema
+        }
+      });
+
+    app.hooks([
+      resolveSchemas()
+    ]);
+
+    const user = await app.service('users').create({
+      email: 'test@user.com'
+    });
+
+    const message = await app.service('messages').create({
+      text: 'Hi there',
+      userId: user.id
+    });
+
+    assert.deepStrictEqual(message, {
+      text: 'Hi there',
+      userId: 0,
+      id: 0,
+      user: { email: 'test@user.com', id: 0 }
+    });
   });
 });
