e013f98315d550ced6eacffd615c61bb0912b4ba
David Luecke <daff@neyeon.com>
2021-07-11 20:17:58 -0700

    fix(koa): Throw a NotFound Feathers error on missing paths (#2415)

--- packages/koa/src/error-handler.ts
+++ packages/koa/src/error-handler.ts
@@ -1,8 +1,14 @@
+import { NotFound } from '@feathersjs/errors';
 import { FeathersKoaContext } from './declarations';
 
 export const errorHandler = () => async (ctx: FeathersKoaContext, next: () => Promise<any>) => {
   try {
     await next();
+
+    if(ctx.body === undefined) {
+      throw new NotFound('Not Found');
+    }
+    
   } catch (error) {
     ctx.response.status = error.code || 500;
     ctx.body = typeof error.toJSON === 'function' ? error.toJSON() : {
