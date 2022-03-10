fdb1f35bbd0b0273e789df3ce40d03586ba45179
fratzinger <22286818+fratzinger@users.noreply.github.com>
2022-03-10 17:25:56 +0100

    chore(transport-common): Cxport channel, combined & connection (#2576)

--- packages/transport-commons/src/index.ts
+++ packages/transport-commons/src/index.ts
@@ -1,6 +1,13 @@
 import { socket } from './socket';
 import { routing } from './routing';
-import { channels } from './channels';
+import { channels, Channel, CombinedChannel, RealTimeConnection } from './channels';
 
 export * as http from './http';
-export { socket, routing, channels };
+export {
+  socket,
+  routing,
+  channels,
+  Channel,
+  CombinedChannel,
+  RealTimeConnection
+};
