feathersjs/feathers

commit 43d8a082d7e1807f8a431c44a1dbd9b04c3af0d2

fix(authentication-oauth): Fix regression using incorrect callback and redirect_uri (#2631)

packages/authentication-oauth/src/index.ts
@@ -41,14 +41,14 @@ export const setup = (options: OauthSetupSettings) => (app: Application) => {
     }
   }
 
-  const grant = defaultsDeep({}, omit(oauth, ['redirect', 'origins']), {
+  const grant = defaultsDeep({
     defaults: {
       prefix,
-      origin: `${protocol}://${host}`,
-      transport: 'session',
+      origin: `${oauth?.defaults?.protocol ?? protocol}://${oauth?.defaults?.host ?? host}`,
+      transport:  oauth?.defaults?.transport ?? 'session',
       response: ['tokens', 'raw', 'profile']
     }
-  });
+  }, omit(oauth, ['redirect', 'origins']));
 
   const getUrl = (url: string) => {
     const { defaults } = grant;

commit ba701f8620ffe3003469346d8cae197ab273e526

fix(authentication-oauth): Fix bug introduced by PR #2631

packages/authentication-oauth/src/index.ts
@@ -44,6 +44,8 @@ export const setup = (options: OauthSetupSettings) => (app: Application) => {
   }
 
   const grant = defaultsDeep(
+    {},
+    omit(oauth, ['redirect', 'origins']),
     {
       defaults: {
         prefix,
@@ -51,8 +53,7 @@ export const setup = (options: OauthSetupSettings) => (app: Application) => {
         transport: oauth?.defaults?.transport ?? 'session',
         response: ['tokens', 'raw', 'profile']
       }
-    },
-    omit(oauth, ['redirect', 'origins'])
+    }
   )
 
   const getUrl = (url: string) => {