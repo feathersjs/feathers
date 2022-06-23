56f828fa85115c8545bce4fe71a7c67ddf3fe582
daffl <daff@neyeon.com>
2022-06-23 14:48:03 -0700

    chore: Remove a unnecessary files

--- app/src/services/index.ts
+++
@@ -1,6 +0,0 @@
-import { testing } from './testing'
-import { Application } from '../declarations'
-
-export default (app: Application) => {
-  app.configure(testing)
-}
--- packages/memory/_templates/memory/service/js/import.js.t
+++
@@ -1,6 +0,0 @@
----
-to: "<%= h.lib %>/services/<%= path %>.js"
-inject: true
-prepend: true
----
-import { Service } from '@feathersjs/memory';
