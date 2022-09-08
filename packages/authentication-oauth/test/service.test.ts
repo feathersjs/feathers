9d4402305b3b374ed46a4e0dc96af1b280c45542
daffl <daff@neyeon.com>
2022-09-08 10:17:16 +0200

    Finalize initial test harness

--- packages/authentication-oauth/test/service.test.ts
+++ packages/authentication-oauth/test/service.test.ts
@@ -1,5 +1,6 @@
 import { strict as assert } from 'assert'
 import axios, { AxiosResponse } from 'axios'
+import { CookieJar } from 'tough-cookie'
 import { expressFixture } from './utils/fixture'
 
 describe('@feathersjs/authentication-oauth service', () => {
@@ -8,6 +9,7 @@ describe('@feathersjs/authentication-oauth service', () => {
     withCredentials: true,
     maxRedirects: 0
   })
+  const cookie = new CookieJar()
   let app: Awaited<ReturnType<typeof expressFixture>>
 
   const fetchErrorResponse = async (url: string): Promise<AxiosResponse> => {
@@ -27,21 +29,29 @@ describe('@feathersjs/authentication-oauth service', () => {
     await app.teardown()
   })
 
-  it('triggers oAuth flow', async () => {
-    let location = `http://localhost:${port}/oauth/github`
+  it('runs through the oAuth flow', async () => {
+    const host = `http://localhost:${port}`
+    let location = `${host}/oauth/github`
 
     const oauthResponse = await fetchErrorResponse(location)
     assert.equal(oauthResponse.status, 303)
+
+    oauthResponse.headers['set-cookie']?.forEach((value) => cookie.setCookie(value, host))
 
     location = oauthResponse.data.location
 
     const providerResponse = await fetchErrorResponse(location)
     assert.equal(providerResponse.status, 302)
 
-    // location = providerResponse.headers.location
+    location = providerResponse.headers.location
 
-    // const { data } = await req.get(location)
+    const { data } = await req.get(location, {
+      headers: {
+        cookie: await cookie.getCookieString(host)
+      }
+    })
 
-    // console.log(data)
+    assert.ok(data.accessToken)
+    assert.equal(data.authentication.strategy, 'github')
   })
 })
