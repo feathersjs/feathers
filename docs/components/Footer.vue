79752fa253bfc00a1957434043d12ed97f3bd89b
Marshall Thompson <marshall@creativeideal.net>
2022-10-17 12:15:45 -0600

    feat(footer) fix spacing

--- docs/components/Footer.vue
+++ docs/components/Footer.vue
@@ -15,29 +15,25 @@ const addLeftMargin = computed(() => {
 const aboutList = [
   {
     label: 'Philosophy',
-    link:
-      'https://blog.feathersjs.com/why-we-built-the-best-web-framework-you-ve-probably-never-heard-of-until-now-176afc5c6aac',
+    link: 'https://blog.feathersjs.com/why-we-built-the-best-web-framework-you-ve-probably-never-heard-of-until-now-176afc5c6aac'
   },
   { label: 'Comparison', link: '/comparison' },
-  { label: 'Ecosystem', link: 'https://github.com/feathersjs/awesome-feathersjs' },
+  { label: 'Ecosystem', link: 'https://github.com/feathersjs/awesome-feathersjs' }
 ]
 const learnList = [
   { label: 'Guides', link: '/guides/' },
   { label: 'API', link: '/api/' },
-  { label: 'Blog', link: 'https://blog.feathersjs.com' },
+  { label: 'Blog', link: 'https://blog.feathersjs.com' }
 ]
 const ecosystemList = [
   { label: 'Become a Backer', link: 'https://github.com/sponsors/daffl' },
   { label: 'Find Help', link: '/help/' },
-  { label: 'Github Issues', link: 'https://github.com/feathersjs/feathers/issues' },
+  { label: 'Github Issues', link: 'https://github.com/feathersjs/feathers/issues' }
 ]
 </script>
 
 <template>
-  <footer
-    class="feathers-footer bg-neutral text-neutral-content"
-    :class="{ 'sidebar-open': addLeftMargin }"
-  >
+  <footer class="feathers-footer bg-neutral text-neutral-content" :class="{ 'sidebar-open': addLeftMargin }">
     <div class="max-w-6xl w-full mx-auto pt-16 px-4">
       <div class="sidebar-adjust">
         <div class="title flex flex-row items-center ml-2">
@@ -45,10 +41,10 @@ const ecosystemList = [
           feathers
         </div>
 
-        <div class="flex flex-row flex-wrap mt-2">
-          <FooterList title="About" :items="aboutList" class="m-6" />
-          <FooterList title="Learn" :items="learnList" class="m-6" />
-          <FooterList title="Ecosystem" :items="ecosystemList" class="m-6" />
+        <div class="flex flex-col sm:flex-row gap-12 sm:gap-0 flex-wrap mt-6 ml-10">
+          <FooterList title="About" :items="aboutList" class="w-1/3" />
+          <FooterList title="Learn" :items="learnList" class="w-1/3" />
+          <FooterList title="Ecosystem" :items="ecosystemList" class="w-1/3" />
         </div>
 
         <div class="h-1 bg-primary rounded-full mt-16"></div>
