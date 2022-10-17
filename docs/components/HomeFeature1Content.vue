aa13c6aaaae833420c2b2afff730a31c7d249317
daffl <daff@neyeon.com>
2022-10-17 10:28:56 -0700

    Minor page edits

--- docs/components/HomeFeature1Content.vue
+++ docs/components/HomeFeature1Content.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="py-6 md:py-0 px-6 md:text-sm prose text-center md:text-left xl:text-xl">
     <p>
-      Feathers is a lightweight web-framework for creating real-time applications and REST
-      APIs using JavaScript or TypeScript.
+      Feathers is a lightweight web-framework for creating APIs and real-time applications using TypeScript or JavaScript.
     </p>
     <p>
-      Feathers can interact with any backend technology, supports over a dozen databases
+      Feathers can interact with any backend technology, supports many databases out of the box
       and works with any frontend technology like React, VueJS, Angular, React Native,
       Android or iOS.
     </p>
