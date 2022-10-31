cccacf0705252db583f28765b22d2945549d44fa
apollo79 <bummtschack@mail.de>
2022-10-31 11:39:38 +0100

    port memory

---
+++ main/memory/README.md
@@ -0,0 +1,114 @@
+# @feathersjs/memory
+
+[![CI](https://github.com/feathersjs/feathers/workflows/CI/badge.svg)](https://github.com/feathersjs/feathers/actions?query=workflow%3ACI)
+[![Download Status](https://img.shields.io/npm/dm/@feathersjs/memory.svg?style=flat-square)](https://www.npmjs.com/package/@feathersjs/memory)
+[![Discord](https://badgen.net/badge/icon/discord?icon=discord&label)](https://discord.gg/qa8kez8QBx)
+
+A [Feathers](https://feathersjs.com) service adapter for in-memory data storage
+that works on all platforms.
+
+```bash
+$ npm install --save @feathersjs/memory
+```
+
+> **Important:** `@feathersjs/memory` implements the
+> [Feathers Common database adapter API](https://docs.feathersjs.com/api/databases/common.html)
+> and
+> [querying syntax](https://docs.feathersjs.com/api/databases/querying.html).
+
+## API
+
+### `service([options])`
+
+Returns a new service instance initialized with the given options.
+
+```js
+const service = require("@feathersjs/memory");
+
+app.use("/messages", service());
+app.use("/messages", service({ id, startId, store, events, paginate }));
+```
+
+**Options:**
+
+- `id` (_optional_, default: `'id'`) - The name of the id field property.
+- `startId` (_optional_, default: `0`) - An id number to start with that will be
+  incremented for every new record (unless it is already set).
+- `store` (_optional_) - An object with id to item assignments to pre-initialize
+  the data store
+- `events` (_optional_) - A list of
+  [custom service events](https://docs.feathersjs.com/api/events.html#custom-events)
+  sent by this service
+- `paginate` (_optional_) - A
+  [pagination object](https://docs.feathersjs.com/api/databases/common.html#pagination)
+  containing a `default` and `max` page size
+- `whitelist` (_DEPRECATED_) - renamed to `allow`
+- `allow` (_optional_) - A list of additional query parameters to allow
+- `multi` (_optional_) - Allow `create` with arrays and `update` and `remove`
+  with `id` `null` to change multiple items. Can be `true` for all methods or an
+  array of allowed methods (e.g. `[ 'remove', 'create' ]`)
+
+## Example
+
+Here is an example of a Feathers server with a `messages` in-memory service that
+supports pagination:
+
+```
+$ npm install @feathersjs/feathers @feathersjs/express @feathersjs/socketio @feathersjs/errors @feathersjs/memory
+```
+
+In `app.js`:
+
+```js
+const feathers = require("@feathersjs/feathers");
+const express = require("@feathersjs/express");
+const socketio = require("@feathersjs/socketio");
+
+const memory = require("@feathersjs/memory");
+
+// Create an Express compatible Feathers application instance.
+const app = express(feathers());
+// Turn on JSON parser for REST services
+app.use(express.json());
+// Turn on URL-encoded parser for REST services
+app.use(express.urlencoded({ extended: true }));
+// Enable REST services
+app.configure(express.rest());
+// Enable REST services
+app.configure(socketio());
+// Create an in-memory Feathers service with a default page size of 2 items
+// and a maximum size of 4
+app.use(
+  "/messages",
+  memory({
+    paginate: {
+      default: 2,
+      max: 4,
+    },
+  }),
+);
+// Set up default error handler
+app.use(express.errorHandler());
+
+// Create a dummy Message
+app.service("messages").create({
+  text: "Message created on server",
+}).then((message) => console.log("Created message", message));
+
+// Start the server.
+const port = 3030;
+
+app.listen(port, () => {
+  console.log(`Feathers server listening on port ${port}`);
+});
+```
+
+Run the example with `node app` and go to
+[localhost:3030/messages](http://localhost:3030/messages).
+
+## License
+
+Copyright (c) 2022
+[Feathers contributors](https://github.com/feathersjs/feathers/graphs/contributors)
+
+Licensed under the [MIT license](LICENSE).
---
+++ main/memory/src/index.ts
@@ -0,0 +1,266 @@
+import { NotFound } from "../../errors/mod.ts";
+import { _ } from "../../commons/mod.ts";
+import {
+  AdapterBase,
+  AdapterParams,
+  AdapterServiceOptions,
+  PaginationOptions,
+  select,
+  sorter,
+} from "../../adapter-commons/mod.ts";
+import sift from "https://esm.sh/sift@15.1.0";
+import {
+  Id,
+  NullableId,
+  Paginated,
+  Params,
+  ServiceMethods,
+} from "../../feathers/mod.ts";
+
+export interface MemoryServiceStore<T> {
+  [key: string]: T;
+}
+
+export interface MemoryServiceOptions<T = any> extends AdapterServiceOptions {
+  store?: MemoryServiceStore<T>;
+  startId?: number;
+  matcher?: (query: any) => any;
+  sorter?: (sort: any) => any;
+}
+
+const _select = (data: any, params: any, ...args: any[]) => {
+  const base = select(params, ...args);
+
+  return base(JSON.parse(JSON.stringify(data)));
+};
+
+export class MemoryAdapter<T = any, D = Partial<T>, P extends Params = Params>
+  extends AdapterBase<
+    T,
+    D,
+    P,
+    MemoryServiceOptions<T>
+  > {
+  store: MemoryServiceStore<T>;
+  _uId: number;
+
+  constructor(options: MemoryServiceOptions<T> = {}) {
+    super({
+      id: "id",
+      matcher: sift,
+      sorter,
+      store: {},
+      startId: 0,
+      ...options,
+    });
+    this._uId = this.options.startId!;
+    this.store = { ...this.options.store };
+  }
+
+  async getEntries(_params?: P) {
+    const params = _params || ({} as P);
+
+    return this.$find({
+      ...params,
+      paginate: false,
+    });
+  }
+
+  getQuery(params: P) {
+    const { $skip, $sort, $limit, $select, ...query } = params.query || {};
+
+    return {
+      query,
+      filters: { $skip, $sort, $limit, $select },
+    };
+  }
+
+  async $find(
+    _params?: P & { paginate?: PaginationOptions },
+  ): Promise<Paginated<T>>;
+  async $find(_params?: P & { paginate: false }): Promise<T[]>;
+  async $find(_params?: P): Promise<Paginated<T> | T[]>;
+  async $find(params: P = {} as P): Promise<Paginated<T> | T[]> {
+    const { paginate } = this.getOptions(params);
+    const { query, filters } = this.getQuery(params);
+
+    let values = _.values(this.store).filter(this.options.matcher!(query));
+    const total = values.length;
+
+    if (filters.$sort !== undefined) {
+      values.sort(this.options.sorter!(filters.$sort));
+    }
+
+    if (filters.$skip !== undefined) {
+      values = values.slice(filters.$skip);
+    }
+
+    if (filters.$limit !== undefined) {
+      values = values.slice(0, filters.$limit);
+    }
+
+    const result: Paginated<T> = {
+      total,
+      limit: filters.$limit,
+      skip: filters.$skip || 0,
+      data: values.map((value) => _select(value, params)),
+    };
+
+    if (!paginate) {
+      return result.data;
+    }
+
+    return result;
+  }
+
+  async $get(id: Id, params: P = {} as P): Promise<T> {
+    const { query } = this.getQuery(params);
+
+    if (id in this.store) {
+      const value = this.store[id];
+
+      if (this.options.matcher!(query)(value)) {
+        return _select(value, params, this.id);
+      }
+    }
+
+    throw new NotFound(`No record found for id '${id}'`);
+  }
+
+  async $create(data: Partial<D>, params?: P): Promise<T>;
+  async $create(data: Partial<D>[], params?: P): Promise<T[]>;
+  async $create(data: Partial<D> | Partial<D>[], _params?: P): Promise<T | T[]>;
+  async $create(
+    data: Partial<D> | Partial<D>[],
+    params: P = {} as P,
+  ): Promise<T | T[]> {
+    if (Array.isArray(data)) {
+      return Promise.all(data.map((current) => this.$create(current, params)));
+    }
+
+    const id = (data as any)[this.id!] || this._uId++;
+    const current = _.extend({}, data, { [this.id!]: id });
+    const result = (this.store[id] = current);
+
+    return _select(result, params, this.id) as T;
+  }
+
+  async $update(id: Id, data: D, params: P = {} as P): Promise<T> {
+    const oldEntry = await this.$get(id);
+    // We don't want our id to change type if it can be coerced
+    const oldId = (oldEntry as any)[this.id!];
+
+    // eslint-disable-next-line eqeqeq
+    id = oldId == id ? oldId : id;
+
+    this.store[id] = _.extend({}, data, { [this.id!]: id });
+
+    return this.$get(id, params);
+  }
+
+  async $patch(id: null, data: Partial<D>, params?: P): Promise<T[]>;
+  async $patch(id: Id, data: Partial<D>, params?: P): Promise<T>;
+  async $patch(id: NullableId, data: Partial<D>, _params?: P): Promise<T | T[]>;
+  async $patch(
+    id: NullableId,
+    data: Partial<D>,
+    params: P = {} as P,
+  ): Promise<T | T[]> {
+    const { query } = this.getQuery(params);
+    const patchEntry = (entry: T) => {
+      const currentId = (entry as any)[this.id!];
+
+      this.store[currentId] = _.extend(
+        this.store[currentId],
+        _.omit(data, this.id!),
+      );
+
+      return _select(this.store[currentId], params, this.id);
+    };
+
+    if (id === null) {
+      const entries = await this.getEntries({
+        ...params,
+        query,
+      });
+
+      return entries.map(patchEntry);
+    }
+
+    return patchEntry(await this.$get(id, params)); // Will throw an error if not found
+  }
+
+  async $remove(id: null, params?: P): Promise<T[]>;
+  async $remove(id: Id, params?: P): Promise<T>;
+  async $remove(id: NullableId, _params?: P): Promise<T | T[]>;
+  async $remove(id: NullableId, params: P = {} as P): Promise<T | T[]> {
+    const { query } = this.getQuery(params);
+
+    if (id === null) {
+      const entries = await this.getEntries({
+        ...params,
+        query,
+      });
+
+      return Promise.all(
+        entries.map((current: any) =>
+          this.$remove(current[this.id!] as Id, params)
+        ),
+      );
+    }
+
+    const entry = await this.$get(id, params);
+
+    delete this.store[id];
+
+    return entry;
+  }
+}
+
+export class MemoryService<
+  T = any,
+  D = Partial<T>,
+  P extends AdapterParams = AdapterParams,
+> extends MemoryAdapter<T, D, P>
+  implements ServiceMethods<T | Paginated<T>, D, P> {
+  async find(
+    params?: P & { paginate?: PaginationOptions },
+  ): Promise<Paginated<T>>;
+  async find(params?: P & { paginate: false }): Promise<T[]>;
+  async find(params?: P): Promise<Paginated<T> | T[]>;
+  async find(params?: P): Promise<Paginated<T> | T[]> {
+    return this._find(params) as any;
+  }
+
+  async get(id: Id, params?: P): Promise<T> {
+    return this._get(id, params);
+  }
+
+  async create(data: D, params?: P): Promise<T>;
+  async create(data: D[], params?: P): Promise<T[]>;
+  async create(data: D | D[], params?: P): Promise<T | T[]> {
+    return this._create(data, params);
+  }
+
+  async update(id: Id, data: D, params?: P): Promise<T> {
+    return this._update(id, data, params);
+  }
+
+  async patch(id: Id, data: Partial<D>, params?: P): Promise<T>;
+  async patch(id: null, data: Partial<D>, params?: P): Promise<T[]>;
+  async patch(id: NullableId, data: Partial<D>, params?: P): Promise<T | T[]> {
+    return this._patch(id, data, params);
+  }
+
+  async remove(id: Id, params?: P): Promise<T>;
+  async remove(id: null, params?: P): Promise<T[]>;
+  async remove(id: NullableId, params?: P): Promise<T | T[]> {
+    return this._remove(id, params);
+  }
+}
+
+export function memory<T = any, D = Partial<T>, P extends Params = Params>(
+  options: Partial<MemoryServiceOptions<T>> = {},
+) {
+  return new MemoryService<T, D, P>(options);
+}
