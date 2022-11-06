7031b813e810307046101f74027806e728e4cbf3
Marshall Thompson <marshall@creativeideal.net>
2022-11-05 23:32:44 -0600

    docs(home) animate the moon and sun

--- docs/components/HomeHero.vue
+++ docs/components/HomeHero.vue
@@ -17,36 +17,65 @@ onBeforeUnmount(toggleHomeClass)
       <div class="feathers-home-hero relative">
         <div class="relative aspect-15/9 overflow-hidden">
           <img
-            class="hero-bg-light w-screen transition-opacity duration-800"
-            :class="[isDark ? 'dark:opacity-0' : 'opacity-100 ']"
-            src="/img/illustration/birds-hero-day.svg"
-            alt="The FeathersJS Birds enjoying a sunny day in the valley of Feathers where a lovely treehouse sits next to the river running between the mountains."
+            class="hero-bg-light w-screen transition-opacity duration-1000 pointer-events-none"
+            src="/img/illustration/hero-day-bg.svg"
+            alt="A sunny daytime background looking over FeathersJS Valley."
             width="1504"
             height="902"
           />
           <img
-            class="hero-bg-dark w-screen absolute inset-0 transition-opacity duration-800"
-            :class="[isDark ? 'dark:opacity-100' : 'opacity-0 ']"
-            src="/img/illustration/birds-hero-night.svg"
+            class="hero-bg-dark w-screen absolute inset-0 transition-opacity duration-1000 pointer-events-none"
+            :class="[isDark ? 'opacity-100' : 'opacity-0 ']"
+            src="/img/illustration/hero-night-bg.svg"
+            alt="A chill nighttime background overlooking FeathersJS Valley"
+            width="1504"
+            height="902"
+          />
+          <img
+            class="hero-bg-dark w-screen absolute inset-0 transition-all duration-1000 pointer-events-none"
+            :class="[isDark ? 'top-[300%] opacity-0' : '-top-[69%] opacity-100']"
+            src="/img/illustration/hero-day-sun-solo.svg"
+            alt="The FeathersJS Birds enjoying a cool night in the valley of Feathers where a lovely treehouse sits next to the river running between the mountains."
+            width="1583"
+            height="1580"
+          />
+          <img
+            class="hero-bg-dark w-screen absolute inset-0 transition-all duration-1000 pointer-events-none"
+            :class="[isDark ? '-top-[69%] opacity-100' : 'top-[300%] opacity-0']"
+            src="/img/illustration/hero-night-moon-solo.svg"
+            alt="The FeathersJS Birds enjoying a cool night in the valley of Feathers where a lovely treehouse sits next to the river running between the mountains."
+            width="1583"
+            height="1580"
+          />
+          <img
+            class="hero-bg-dark w-screen absolute inset-0 transition-opacity duration-1000 pointer-events-none"
+            src="/img/illustration/hero-day-foreground.svg"
             alt="The FeathersJS Birds enjoying a cool night in the valley of Feathers where a lovely treehouse sits next to the river running between the mountains."
             width="1504"
             height="902"
           />
-        </div>
-
-        <div class="absolute inset-0 flex flex-col pt-13.5vw md:pt-14.5vw">
-          <div
-            class="transition-all duration-400 text-center text-lg xl:text-6xl leading-tight font-bold"
-            sm="text-2xl"
-            md="text-3xl"
-            lg="text-5xl"
-            xl="text-6xl"
-          >
-            The API and Real-time <br />
-            Application Framework
-          </div>
-          <div class="mx-auto mt-4 lg:mt-12">
-            <CTAButton primary>Get Started</CTAButton>
+          <img
+            class="hero-bg-dark w-screen absolute inset-0 transition-opacity duration-1000 pointer-events-none"
+            :class="[isDark ? 'opacity-100' : 'opacity-0 ']"
+            src="/img/illustration/hero-night-foreground.svg"
+            alt="The FeathersJS Birds enjoying a cool night in the valley of Feathers where a lovely treehouse sits next to the river running between the mountains."
+            width="1504"
+            height="902"
+          />
+          <div class="absolute inset-0 flex flex-col pt-13.5vw md:pt-14.5vw">
+            <div
+              class="transition-all duration-400 text-center text-lg xl:text-6xl leading-tight font-bold"
+              sm="text-2xl"
+              md="text-3xl"
+              lg="text-5xl"
+              xl="text-6xl"
+            >
+              The API and Real-time <br />
+              Application Framework
+            </div>
+            <div class="mx-auto mt-4 lg:mt-12">
+              <CTAButton primary>Get Started</CTAButton>
+            </div>
           </div>
         </div>
       </div>
