1d83eaad0d053d63fd9464b14630eab8c9135ca5
daffl <daff@neyeon.com>
2022-12-19 17:03:59 -0800

    Revert MongoDB adapter change

--- packages/mongodb/src/adapter.ts
+++ packages/mongodb/src/adapter.ts
@@ -281,7 +281,9 @@ export class MongoDbAdapter<
       ? model
           .insertMany(data.map(setId), writeOptions)
           .then(async (result) =>
-            model.find({ _id: { $in: Object.values(result.insertedIds) } }, params.mongodb).toArray()
+            Promise.all(
+              Object.values(result.insertedIds).map(async (_id) => model.findOne({ _id }, params.mongodb))
+            )
           )
       : model
           .insertOne(setId(data), writeOptions)
