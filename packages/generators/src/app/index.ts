760890fdbb09fd981d3fb09e2e72af5950203a09
Ray Foss <ray@vblip.com>
2023-03-25 16:01:29 -0500

    Sort front end clients list by popularity

--- packages/generators/src/app/index.ts
+++ packages/generators/src/app/index.ts
@@ -111,7 +111,7 @@ export const generate = (ctx: AppGeneratorArguments) =>
           type: 'confirm',
           when: ctx.client === undefined,
           message: (answers) => `Generate ${answers.language === 'ts' ? 'end-to-end typed ' : ''}client?`,
-          suffix: chalk.grey(' Can be used with React, Angular, Vue, React Native, Node.js etc.')
+          suffix: chalk.grey(' Can be used with React, Vue, React Native, Angular, Node.js etc.')
         },
         {
           type: 'list',
