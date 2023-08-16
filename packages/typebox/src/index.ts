e7ce131cf1334ca91a177ff040bb0d6bed69cf16
daffl <daff@neyeon.com>
2023-08-16 10:04:41 -0700

    Formatting

--- packages/typebox/src/index.ts
+++ packages/typebox/src/index.ts
@@ -1,4 +1,13 @@
-import { Type, TObject, TInteger, TOptional, TSchema, ObjectOptions, TIntersect, TUnion } from '@sinclair/typebox'
+import {
+  Type,
+  TObject,
+  TInteger,
+  TOptional,
+  TSchema,
+  ObjectOptions,
+  TIntersect,
+  TUnion
+} from '@sinclair/typebox'
 import { jsonSchema, Validator, DataValidatorMap, Ajv } from '@feathersjs/schema'
 
 export * from '@sinclair/typebox'
@@ -17,8 +26,10 @@ export type TDataSchemaMap = {
  * @param validator The AJV validation instance
  * @returns A compiled validation function
  */
-export const getValidator = <T = any, R = T>(schema: TObject | TIntersect | TUnion<TObject[]>, validator: Ajv): Validator<T, R> =>
-  jsonSchema.getValidator(schema as any, validator)
+export const getValidator = <T = any, R = T>(
+  schema: TObject | TIntersect | TUnion<TObject[]>,
+  validator: Ajv
+): Validator<T, R> => jsonSchema.getValidator(schema as any, validator)
 
 /**
  * Returns compiled validation functions to validate data for the `create`, `update` and `patch`
