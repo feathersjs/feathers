677c214a353a7f9a1f90649b9bbec4d0d6517a6f
David Luecke <daff@neyeon.com>
2023-11-27 14:58:22 -0800

    fix(schema): Allow $in and $nin queries to work for arrays (#3352)

--- packages/typebox/src/index.ts
+++ packages/typebox/src/index.ts
@@ -110,8 +110,8 @@ export const queryProperty = <T extends TSchema, X extends { [key: string]: TSch
               $lt: def,
               $lte: def,
               $ne: def,
-              $in: Type.Array(def),
-              $nin: Type.Array(def)
+              $in: def.type === 'array' ? def : Type.Array(def),
+              $nin: def.type === 'array' ? def : Type.Array(def)
             }),
             Type.Object(extension)
           ],
