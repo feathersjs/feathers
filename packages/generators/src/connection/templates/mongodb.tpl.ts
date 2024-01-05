a74f66a6cfc22c4c08a6c9a7ded3c0ffaa4e67fc
daffl <daff@neyeon.com>
2024-01-05 15:11:00 -0800

    chore: Revert "fix(generators): Harden mongodb.js to reliably extract database from any connection string (#3264)"
    This reverts commit 7b0f82c631ff5549cdc9a8e0ffcc705d067c2157.

--- packages/generators/src/connection/templates/mongodb.tpl.ts
+++ packages/generators/src/connection/templates/mongodb.tpl.ts
@@ -17,14 +17,10 @@ declare module './declarations' {
 
 export const ${database} = (app: Application) => {
   const connection = app.get('${database}') as string
-  
-  const databaseRegex = /\/([a-zA-Z0-9_\\-]+)(?:\?|$)/;
-
-  const databaseName = databaseRegex.exec(connection)?.[1];
-  
+  const database = new URL(connection).pathname.substring(1)
   const mongoClient = MongoClient.connect(connection)
-    .then(client => client.db(databaseName));
-    
+    .then(client => client.db(database))
+
   app.set('${database}Client', mongoClient)
 }
 `
