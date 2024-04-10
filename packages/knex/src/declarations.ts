11ddb35dd6285dcb86d38070ac3ae63d6a344d5d
Ashot Nazaryan <contact@hego.co>
2024-04-10 11:55:22 -0700

    commited is a promise

--- packages/knex/src/declarations.ts
+++ packages/knex/src/declarations.ts
@@ -10,7 +10,7 @@ export interface KnexAdapterOptions extends AdapterServiceOptions {
 export interface KnexAdapterTransaction {
   starting: boolean
   parent?: KnexAdapterTransaction
-  committed?: boolean
+  committed?: Promise<boolean>
   resolve?: any
   trx?: Knex.Transaction
   id?: number
