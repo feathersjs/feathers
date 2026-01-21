05ebb65f91aed912ef12645697be01a0334c388f
Marshall Thompson <marshall@creativeideal.net>
2026-01-21 02:36:08 -0700

    feat(website): Convert GitHub button to icon button

--- website/app/components/TopNav.vue
+++ website/app/components/TopNav.vue
@@ -25,13 +25,16 @@ const { openSearch } = useGlobalSearch()
           <Icon name="heroicons:magnifying-glass" class="w-5 h-5" />
           <span class="hidden lg:inline">Search</span>
         </Button>
-        <NuxtLink
-          class="btn btn-primary hidden sm:flex"
-          to="https://github.com/feathersjs/feathers"
+        <Button
+          ghost
+          circle
+          is="NuxtLink"
+          href="https://github.com/feathersjs/feathers"
           target="_blank"
+          aria-label="GitHub"
         >
-          GitHub
-        </NuxtLink>
+          <Icon name="mdi:github" size="24" />
+        </Button>
       </NavbarEnd>
     </Navbar>
   </Flex>
