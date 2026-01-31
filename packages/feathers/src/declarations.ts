854c50130a69debe012f078824a79b82eb0fdacf
David Luecke <daff@neyeon.com>
2026-01-30 17:49:12 -0800

    chore: Update all dependencies (#3646)

--- packages/feathers/src/declarations.ts
+++ packages/feathers/src/declarations.ts
@@ -318,8 +318,7 @@ export interface FeathersApplication<Services = any, Settings = any> {
 // This needs to be an interface instead of a type
 // so that the declaration can be extended by other modules
 export interface Application<Services = any, Settings = any>
-  extends FeathersApplication<Services, Settings>,
-    EventEmitter {}
+  extends FeathersApplication<Services, Settings>, EventEmitter {}
 
 export type Id = number | string
 export type NullableId = Id | null
