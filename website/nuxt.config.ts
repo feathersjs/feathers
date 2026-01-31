75b860a6f2af17d4ee603299ae9bb6e62e448862
Marshall Thompson <marshall@creativeideal.net>
2026-01-30 20:40:16 -0700

    docs: fix docs refresh, missing components, search (#3649)

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -2,9 +2,7 @@
 export default defineNuxtConfig({
   extends: ['@feathersdev/websites'],
 
-  nitro: {
-    preset: 'cloudflare-module'
-  },
+  modules: ['daisy-ui-kit/nuxt'],
 
   app: {
     head: {
@@ -22,5 +20,9 @@ export default defineNuxtConfig({
 
   devServer: {
     port: 3009
+  },
+
+  nitro: {
+    preset: 'cloudflare-module'
   }
 })
