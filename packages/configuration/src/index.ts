477bf45f9c9dbde77a14a07828aa02300de23ae7
David Luecke <daff@neyeon.com>
2026-03-04 13:57:11 -0800

    fix: Update dependencies (#3666)

--- packages/configuration/src/index.ts
+++ packages/configuration/src/index.ts
@@ -5,7 +5,7 @@ import config from 'config'
 
 const debug = createDebug('@feathersjs/configuration')
 
-export = function init(schema?: Schema<any> | Validator) {
+export = function init(schema?: Schema<any> | Validator): (app?: Application) => Record<string, any> {
   const validator: Validator = typeof schema === 'function' ? schema : schema?.validate.bind(schema)
 
   return (app?: Application) => {
