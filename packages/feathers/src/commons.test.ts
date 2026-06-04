970e3f7bf0a02c41fa9d8e58191dafad02dbd94f
Marshall Thompson <marshall@creativeideal.net>
2026-06-03 19:46:11 -0600

    fix(feathers): skip prototype-polluting keys in _.merge
    Object.keys() returns __proto__ as an own enumerable key for
    JSON-parsed sources, causing the recursive merge to write onto
    Object.prototype. Skip __proto__/constructor/prototype keys.
    
    Reported-by: Andrew Ridings (@ridingsa)

--- packages/feathers/src/commons.test.ts
+++ packages/feathers/src/commons.test.ts
@@ -213,5 +213,14 @@ describe('feathers/commons utils', () => {
 
       assert.equal(_.merge('hello', {}), 'hello')
     })
+
+    it('merge does not pollute Object.prototype', () => {
+      _.merge({}, JSON.parse('{"__proto__":{"polluted":"x"}}'))
+      _.merge({}, JSON.parse('{"constructor":{"prototype":{"polluted2":"y"}}}'))
+      assert.strictEqual(({} as any).polluted, undefined)
+      assert.strictEqual(({} as any).polluted2, undefined)
+      assert.strictEqual((Object.prototype as any).polluted, undefined)
+      assert.strictEqual((Object.prototype as any).polluted2, undefined)
+    })
   })
 })
